05381ff24a55e0de57f1895cec36db83396518e6
Robert Weber <robert.weber@logrhythm.com>
2013-03-21 11:29:15 -0600

    this call is not ok in production mode

--- QosmosDpiTest.cpp
+++ QosmosDpiTest.cpp
@@ -200,7 +200,9 @@ TEST_F(QosmosDpiTest, DisableEnableProtoTest) {
       EXPECT_FALSE( mDpiEngine->FamilyEnable( invalidFamilyName ) );
       EXPECT_FALSE( mDpiEngine->FamilyDisable( invalidFamilyName ) );
        */
+#ifdef LR_DEBUG
       mDpiEngine->ReleaseDevice();
+#endif
    }
 }
 
