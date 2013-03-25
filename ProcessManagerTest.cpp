463879b92fd6223e3900a734431720a145c759a6
Robert Weber <robert.weber@logrhythm.com>
2013-03-25 15:37:39 -0600

    Support for running processes in the ProcessManager

--- ProcessManagerTest.cpp
+++ ProcessManagerTest.cpp
@@ -13,3 +13,26 @@ TEST_F(ProcessManagerTest, ConstructAndInitialize) {
    EXPECT_TRUE(testManager.Initialize());
    raise(SIGTERM);
 }
+TEST_F(ProcessManagerTest, RunProcess) {
+   MockConf conf;
+   ProcessManager testManager(conf);
+   EXPECT_TRUE(testManager.Initialize());
+   std::string processName("/bin/ls");
+   std::string processArgs;
+   processArgs = "-l /bin/ls";
+   std::string result = (testManager.RunProcess(processName,processArgs));
+   EXPECT_NE(std::string::npos,result.find("-rwxr-xr-x. 1 root root"));
+   EXPECT_NE(std::string::npos,result.find("/bin/ls"));
+   raise(SIGTERM);
+}
+TEST_F(ProcessManagerTest, RunNonExistantProcess) {
+   MockConf conf;
+   ProcessManager testManager(conf);
+   EXPECT_TRUE(testManager.Initialize());
+   std::string processName("/bin/lsssss");
+   std::string processArgs;
+   processArgs = "-l /bin/ls";
+   std::string result = (testManager.RunProcess(processName,processArgs));
+   EXPECT_TRUE(result.empty());
+   raise(SIGTERM);
+}
