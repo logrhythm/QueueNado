ca78bf5a7c7d5081bc6dbd539e73a83a5c9aeaf3
Robert Weber <robert.weber@logrhythm.com>
2013-03-26 10:30:16 -0600

    This will test the one object starts, another object uses model

--- ProcessManagerTest.cpp
+++ ProcessManagerTest.cpp
@@ -51,3 +51,38 @@ TEST_F(ProcessManagerTest, RunNonExistantProcess) {
    testManager.DeInit();
 #endif
 }
+
+TEST_F(ProcessManagerTest, RunProcessFromAnotherObject) {
+#ifdef LR_DEBUG
+   MockConf conf;
+   ProcessManager testManager(conf);
+   EXPECT_TRUE(testManager.Initialize());
+   ProcessManager sendManager(conf);
+   EXPECT_TRUE(sendManager.Initialize());
+   std::string processName("/bin/ls");
+   std::string processArgs;
+   processArgs = "-l /bin/ls";
+   std::string result = (sendManager.RunProcess(processName, processArgs));
+   EXPECT_NE(std::string::npos, result.find("-rwxr-xr-x. 1 root root"));
+   EXPECT_NE(std::string::npos, result.find("/bin/ls"));
+   raise(SIGTERM);
+   testManager.DeInit();
+#endif
+}
+
+TEST_F(ProcessManagerTest, RunNonExistantProcessFromAnotherObject) {
+#ifdef LR_DEBUG
+   MockConf conf;
+   ProcessManager testManager(conf);
+   EXPECT_TRUE(testManager.Initialize());
+   ProcessManager sendManager(conf);
+   EXPECT_TRUE(sendManager.Initialize());
+   std::string processName("/bin/lsssss");
+   std::string processArgs;
+   processArgs = "-l /bin/ls";
+   std::string result = (sendManager.RunProcess(processName, processArgs));
+   EXPECT_TRUE(result.empty());
+   raise(SIGTERM);
+   testManager.DeInit();
+#endif
+}
