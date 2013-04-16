3b86e53055bc3bcbdd642773fec9f3a7cdfda199
Ben Aldrich <ben.aldrich@logrhythm.com>
2013-04-16 11:42:28 -0600

    revert test

--- QosmosPacketAllocatorTests.cpp
+++ QosmosPacketAllocatorTests.cpp
@@ -1,5 +1,3 @@
-#include <fstream>
-
 #include "QosmosPacketAllocatorTests.h"
 using namespace std;
 
@@ -75,63 +73,12 @@ TEST_F(QsomosPacketAllocatorTests, FailedMalloc) {
 #endif
 }
 
-TEST_F(QsomosPacketAllocatorTests, TwoPacketConversions) {
-   std::fstream inputFile;
-   std::fstream outputFile;
-   inputFile.open("resources/1000packets.tcpdump", fstream::in | fstream::binary);
-
-   unsigned char* buffer = new unsigned char[65536];
-   inputFile.read(reinterpret_cast<char*>(buffer), sizeof (pcap_file_header));
-   if (!inputFile && inputFile.gcount() < sizeof (struct pcap_file_header)) {
-      inputFile.close();
-      delete [] buffer;
-      ASSERT_TRUE(false);
-   }
-   struct pcap_file_header* fileHeader = reinterpret_cast<struct pcap_file_header*>(buffer);
-   outputFile.open("resources/1000packets.tcpdump.copy",fstream::out | fstream::binary | fstream::trunc);
-   outputFile.write(reinterpret_cast<char*>(buffer),sizeof(struct pcap_file_header));
-   ctb_ppacket dataPointer(NULL);
-   while (!inputFile.eof()) {
-      struct pcap_pkthdr* header = static_cast<struct pcap_pkthdr*>(malloc(sizeof(struct pcap_pkthdr)));
-      inputFile.read(reinterpret_cast<char*>(buffer), sizeof (struct pcap_pkthdr));
-      if (!inputFile && inputFile.gcount() < sizeof (struct pcap_pkthdr)) {
-         break;
-      }
-      memcpy(&header->ts.tv_sec,buffer,sizeof(header->ts.tv_sec));
-      memcpy(&header->ts.tv_usec,buffer+sizeof(header->ts.tv_sec),sizeof(header->ts.tv_usec));
-      header->caplen = buffer[sizeof(header->ts.tv_sec)+sizeof(header->ts.tv_sec)];
-      header->len = header->caplen;
-      size_t length = header->caplen;
-      ASSERT_TRUE(length<=65536);
-      inputFile.read(reinterpret_cast<char*>(buffer), length);
-      if (!inputFile && inputFile.gcount() < length) {
-         break;
-      }
-      unsigned char* rawData = buffer;
-      
-      t_allocator.PopulatePacketData(rawData, header, dataPointer);
-      struct pcap_pkthdr* header2(NULL);
-      unsigned char* raw2(NULL);
-      ASSERT_TRUE(t_allocator.PopulatrePCapData(dataPointer, header2, raw2));
-      outputFile.write(reinterpret_cast<char*>(header2),sizeof(struct pcap_file_header));
-      outputFile.write(reinterpret_cast<char*>(raw2),header2->len);
-      free(header2);
-      free(raw2);
-      free(dataPointer->data);
-      free(dataPointer);
-      free(header);
-   }
-   outputFile.close();
-   inputFile.close();
-   delete [] buffer;
-}
-
 TEST_F(QsomosPacketAllocatorTests, PacketTranslation) {
    std::string* packetAsString;
    ctb_ppacket testPacket = NULL;
    t_allocator.PopulateStringFromPacket(testPacket, packetAsString);
    std::string testPacketString;
-   t_allocator.PopulatePacketFromString(testPacketString, testPacket);
+   t_allocator.PopulatePacketFromString(testPacketString,testPacket);
    EXPECT_TRUE(testPacket == NULL);
    testPacket = reinterpret_cast<ctb_ppacket> (malloc(sizeof (ctb_pkt)));
    testPacket->timestamp.tv_sec = 12;
@@ -143,15 +90,15 @@ TEST_F(QsomosPacketAllocatorTests, PacketTranslation) {
    t_allocator.PopulateStringFromPacket(testPacket, packetAsString);
    EXPECT_TRUE(packetAsString != NULL);
    EXPECT_TRUE(testPacket == NULL);
-   EXPECT_EQ(100 + sizeof (timeval) + sizeof (unsigned int), packetAsString->size());
-   t_allocator.PopulatePacketFromString(*packetAsString, testPacket);
+   EXPECT_EQ(100+sizeof(timeval)+sizeof(unsigned int),packetAsString->size());
+   t_allocator.PopulatePacketFromString(*packetAsString,testPacket);
    delete packetAsString;
    EXPECT_TRUE(testPacket != NULL);
-   EXPECT_EQ(12, testPacket->timestamp.tv_sec);
-   EXPECT_EQ(45, testPacket->timestamp.tv_usec);
-   EXPECT_EQ(100, testPacket->len);
+   EXPECT_EQ(12,testPacket->timestamp.tv_sec);
+   EXPECT_EQ(45,testPacket->timestamp.tv_usec);
+   EXPECT_EQ(100,testPacket->len);
    free(testPacket->data);
    free(testPacket);
-
+   
 
 }
