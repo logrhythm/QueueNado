3d71d2bf9ed705f0eaebfea10f0803a80b90a696
Ben Aldrich <ben.aldrich@logrhythm.com>
2013-06-18 15:25:22 -0600

    add feature to track pids for all processes Reclaim based on pids

--- MockProcessManager.h
+++ MockProcessManager.h
@@ -5,28 +5,50 @@ class MockProcessManager : public ProcessManager {
 public:
 
    MockProcessManager(const Conf& conf) : ProcessManager(conf), mKillFails(false),
-                      mExecFails(false){
+      mExecFails(false) {
+      ProcessManager::SetPidDir("/tmp");
    }
 
    virtual ~MockProcessManager() {
    }
-   
+
    using ProcessManager::StartDaemon;
    using ProcessManager::StopProcess;
 
-   bool KillPid(pid_t pid) {
+   bool KillPid(pid_t pid) override {
       if (mKillFails) {
          return false;
       }
       return ProcessManager::KillPid(pid);
    }
 
-   int RunExecVE(const char* command, char** arguments, char** environment) {
+   int RunExecVE(const char* command, char** arguments, char** environment) override {
       if (mExecFails) {
          return -1;
       }
-      return ProcessManager::RunExecVE(command,arguments,environment);
+      return ProcessManager::RunExecVE(command, arguments, environment);
    }
+
+   virtual bool CheckPidExists(const int pid) override {
+      return ProcessManager::CheckPidExists(pid);
+   }
+   
+   virtual int ReclaimPid(const protoMsg::ProcessRequest& request, const int pid) override {
+      return ProcessManager::ReclaimPid(request, pid);
+   }
+   
+   virtual int GetPidFromFiles(const protoMsg::ProcessRequest& request) override {
+      return ProcessManager::GetPidFromFiles(request);
+   }
+   
+   virtual bool WritePid(const int pid) override {
+      return ProcessManager::WritePid(pid);
+   }
+   
+   virtual bool DeletePid(const int pid) override {
+      return ProcessManager::DeletePid(pid);
+   }
+   
    bool mKillFails;
    bool mExecFails;
 };
