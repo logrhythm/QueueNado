ad2809786a7d742b57c0cc97da65bfa451f3357d
Robert Weber <robert.weber@logrhythm.com>
2013-06-21 16:33:38 -0600

    Some more functionality

--- RESTBuilderTest.cpp
+++ RESTBuilderTest.cpp
@@ -27,17 +27,25 @@ TEST_F(RESTBuilderTest, CreateAndDeleteIndex) {
    std::string reply;
    EXPECT_TRUE(sender.Send(command, reply));
    EXPECT_EQ("{\"ok\":true,\"acknowledged\":true}", reply);
+   EXPECT_TRUE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    transport.mReturnString = "{\"error\":\"IndexAlreadyExistsException[[indexName] Alread exists]\",\"status\":400}";
    EXPECT_FALSE(sender.Send(command, reply));
+   EXPECT_FALSE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    EXPECT_EQ(transport.mReturnString, reply);
    transport.mReturnString.clear();
    command = builder.GetIndexDeletion("indexName");
 
    EXPECT_EQ("DELETE|/indexName/", command);
    EXPECT_TRUE(sender.Send(command, reply));
+   EXPECT_TRUE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    EXPECT_EQ("{\"ok\":true,\"acknowledged\":true}", reply);
    transport.mReturnString = "{\"error\":\"IndexMissingException[[indexName] missing]\",\"status\":404}";
    EXPECT_FALSE(sender.Send(command, reply));
+   EXPECT_FALSE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    EXPECT_EQ(transport.mReturnString, reply);
 }
 
@@ -55,15 +63,64 @@ TEST_F(RESTBuilderTest, OpenAndCloseIndex) {
    std::string reply;
    EXPECT_TRUE(sender.Send(command, reply));
    EXPECT_EQ("{\"ok\":true,\"acknowledged\":true}", reply);
+   EXPECT_TRUE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    transport.mReturnString = "{\"error\":\"IndexMissingException[[indexName] missing]\",\"status\":404}";
    EXPECT_FALSE(sender.Send(command, reply));
+   EXPECT_FALSE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    EXPECT_EQ(transport.mReturnString, reply);
    transport.mReturnString.clear();
    command = builder.GetIndexOpen("indexName");
 
    EXPECT_EQ("POST|/indexName/_open", command);
    EXPECT_TRUE(sender.Send(command, reply));
+   EXPECT_TRUE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
    EXPECT_EQ("{\"ok\":true,\"acknowledged\":true}", reply);
+   transport.mReturnString = "{\"error\":\"IndexMissingException[[indexName] missing]\",\"status\":404}";
+   EXPECT_FALSE(sender.Send(command, reply));
+   EXPECT_FALSE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));
+   EXPECT_EQ(transport.mReturnString, reply);
+}
+
+TEST_F(RESTBuilderTest, AddDocDeleteDoc) {
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+
+   ASSERT_TRUE(transport.Initialize());
+   RESTBuilder builder;
+   RESTSender sender(transport);
+   int shards(3);
+   int replicas(5);
+   std::string command = builder.GetAddDoc("indexName", "typeName", "abc_123", "{\"test\":\"data\"}");
+
+   EXPECT_EQ("PUT|/indexName/typeName/abc_123|{\"test\":\"data\"}", command);
+   std::string reply;
+   transport.mReturnString = "{\"ok\":true,\"_index\":\"indexName\",\"_type\":\"typeName\",\"_id\":\"abc_123\",\"_version\":1}";
+
+   EXPECT_TRUE(sender.Send(command, reply));
+   EXPECT_EQ(transport.mReturnString, reply);
+   transport.mReturnString = "{\"error\":\"IndexMissingException[[indexName] missing]\",\"status\":404}";
+   EXPECT_FALSE(sender.Send(command, reply));
+   EXPECT_EQ(transport.mReturnString, reply);
+
+   transport.mReturnString.clear();
+
+   command = builder.GetDeleteDoc("indexName", "typeName", "abc_123");
+
+   EXPECT_EQ("DELETE|/indexName/typeName/abc_123", command);
+   transport.mReturnString = "{\"ok\":true,\"found\":true,\"_index\":\"indexName\","
+           "\"_type\":\"typeName\",\"_id\":\"abc_123\",\"_version\":1}";
+
+   EXPECT_TRUE(sender.Send(command, reply));
+   EXPECT_FALSE(sender.IsOKAndAck(reply));
+   EXPECT_TRUE(sender.IsOkAndFound(reply));
+   transport.mReturnString = "{\"ok\":true,\"found\":false,\"_index\":\"indexName\","
+           "\"_type\":\"typeName\",\"_id\":\"abc_123\",\"_version\":1}";
+   EXPECT_TRUE(sender.Send(command, reply));
+   EXPECT_FALSE(sender.IsOKAndAck(reply));
+   EXPECT_FALSE(sender.IsOkAndFound(reply));   
    transport.mReturnString = "{\"error\":\"IndexMissingException[[indexName] missing]\",\"status\":404}";
    EXPECT_FALSE(sender.Send(command, reply));
    EXPECT_EQ(transport.mReturnString, reply);
