63ca6399f7d440eb60c8539d10decae746170f24
Robert Weber <robert.weber@logrhythm.com>
2013-07-09 14:31:22 -0600

    Intermediate checkin, fixed the tests, started adding tests for the Index cleanup

--- DiskCleanupTest.cpp
+++ DiskCleanupTest.cpp
@@ -10,7 +10,7 @@ TEST_F(DiskCleanupTest, TooMuchPCap) {
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup1";
       std::atomic<size_t> aDiskUsed(0);
       std::atomic<size_t> aTotalFiles(0);
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       ASSERT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
 
       std::stringstream testDir;
@@ -28,13 +28,16 @@ TEST_F(DiskCleanupTest, TooMuchPCap) {
       system(makeADir.c_str());
 
       ASSERT_EQ(0, system(makeADir.c_str()));
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup2";
+      capture.ResetConf();
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup3";
+      capture.ResetConf();
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup4";
+      capture.ResetConf();
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       std::string makeSmallFile = "touch ";
       makeSmallFile += testDir.str();
@@ -42,9 +45,10 @@ TEST_F(DiskCleanupTest, TooMuchPCap) {
 
       EXPECT_EQ(0, system(makeSmallFile.c_str()));
       std::this_thread::sleep_for(std::chrono::seconds(1));
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup5";
+      capture.ResetConf();
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       std::string make1MFileFile = "dd bs=1024 count=1024 if=/dev/zero of=";
       make1MFileFile += testDir.str();
@@ -52,32 +56,36 @@ TEST_F(DiskCleanupTest, TooMuchPCap) {
 
       EXPECT_EQ(0, system(make1MFileFile.c_str()));
       std::this_thread::sleep_for(std::chrono::seconds(1));
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup6";
+      capture.ResetConf();
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       make1MFileFile = "dd bs=1048576 count=1 if=/dev/zero of=";
       make1MFileFile += testDir.str();
       make1MFileFile += "/1MFilelessone";
       EXPECT_EQ(0, system(make1MFileFile.c_str()));
       std::this_thread::sleep_for(std::chrono::seconds(1));
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup7";
+      capture.ResetConf();
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
 
-      capture.RemoveOldestPCapFile();
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RemoveOldestPCapFiles(1);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup8";
+      capture.ResetConf();
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
-      capture.RemoveOldestPCapFile();
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RemoveOldestPCapFiles(1);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup9";
+      capture.ResetConf();
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
-      capture.RemoveOldestPCapFile();
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RemoveOldestPCapFiles(1);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
 
       makeADir = "";
@@ -96,7 +104,8 @@ TEST_F(DiskCleanupTest, CleanupOldPcapFiles) {
       std::atomic<size_t> aDiskUsed(0);
       std::atomic<size_t> aTotalFiles(0);
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup1";
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.ResetConf();
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       ASSERT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
 
       std::stringstream testDir;
@@ -111,34 +120,36 @@ TEST_F(DiskCleanupTest, CleanupOldPcapFiles) {
       makeADir += testDir.str();
       system(makeADir.c_str());
 
-      //      capture.Initialize();
       std::string makeSmallFile = "touch ";
       makeSmallFile += testDir.str();
       makeSmallFile += "/smallFile";
       EXPECT_EQ(0, system(makeSmallFile.c_str()));
       std::this_thread::sleep_for(std::chrono::seconds(1));
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       std::string make1MFileFile = "dd bs=1024 count=1024 if=/dev/zero of=";
       make1MFileFile += testDir.str();
       make1MFileFile += "/1MFile";
       EXPECT_EQ(0, system(make1MFileFile.c_str()));
       std::this_thread::sleep_for(std::chrono::seconds(1));
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       make1MFileFile = "dd bs=1048576 count=1 if=/dev/zero of=";
       make1MFileFile += testDir.str();
       make1MFileFile += "/1MFilelessone";
       EXPECT_EQ(0, system(make1MFileFile.c_str()));
       std::this_thread::sleep_for(std::chrono::seconds(1));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup7";
-      capture.RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+      capture.ResetConf();
+      capture.RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       capture.CleanupOldPcapFiles(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup8";
+      capture.ResetConf();
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       capture.CleanupOldPcapFiles(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       mConf.mConfLocation = "resources/test.yaml.DiskCleanup9";
+      capture.ResetConf();
       EXPECT_TRUE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
       capture.CleanupOldPcapFiles(aDiskUsed, aTotalFiles);
       EXPECT_FALSE(capture.TooMuchPCap(aDiskUsed, aTotalFiles));
@@ -150,3 +161,16 @@ TEST_F(DiskCleanupTest, CleanupOldPcapFiles) {
    }
 #endif
 }
+#ifdef LR_DEBUG
+
+TEST_F(DiskCleanupTest, TooMuchSearch) {
+
+   MockDiskCleanup diskCleanup(mConf);
+
+   EXPECT_TRUE(diskCleanup.TooMuchSearch(0, 100));
+   EXPECT_TRUE(diskCleanup.TooMuchSearch(14, 100));
+   EXPECT_FALSE(diskCleanup.TooMuchSearch(15, 100));
+   EXPECT_FALSE(diskCleanup.TooMuchSearch(100, 100));
+   EXPECT_FALSE(diskCleanup.TooMuchSearch(1000, 100));
+}
+#endif
--- MockDiskCleanup.h
+++ MockDiskCleanup.h
@@ -10,23 +10,27 @@ public:
    virtual ~MockDiskCleanup() {
    }
 
-   void RemoveOldestPCapFile() {
+   void RemoveOldestPCapFiles(const size_t maxToRemove) override {
       DiskCleanup::RemoveOldestPCapFiles(1);
    }
 
-   void RemoveOldestPCapFiles(const size_t max) {
-      DiskCleanup::RemoveOldestPCapFiles(1);
-   }
-
-   bool TooMuchPCap(std::atomic<size_t>& aDiskUsed, std::atomic<size_t>& aTotalFiles) {
+   bool TooMuchPCap(std::atomic<size_t>& aDiskUsed, std::atomic<size_t>& aTotalFiles) override {
       return DiskCleanup::TooMuchPCap(aDiskUsed, aTotalFiles);
    }
 
-   void RecalculateDiskUsed(std::atomic<size_t>& aDiskUsed, std::atomic<size_t>& aTotalFiles) {
-      DiskCleanup::RecalculateDiskUsed(aDiskUsed, aTotalFiles);
+   void RecalculatePCapDiskUsed(std::atomic<size_t>& aDiskUsed, std::atomic<size_t>& aTotalFiles) override {
+      DiskCleanup::RecalculatePCapDiskUsed(aDiskUsed, aTotalFiles);
    }
 
-   void CleanupOldPcapFiles(std::atomic<size_t>& aDiskUsed, std::atomic<size_t>& aTotalFiles) {
+   void CleanupOldPcapFiles(std::atomic<size_t>& aDiskUsed, std::atomic<size_t>& aTotalFiles) override {
       DiskCleanup::CleanupOldPcapFiles(aDiskUsed, aTotalFiles);
+   }
+
+   bool TooMuchSearch(const size_t& fsFreeGigs, const size_t& fsTotalGigs) override {
+      return DiskCleanup::TooMuchSearch(fsFreeGigs, fsTotalGigs);
+   }
+
+   void ResetConf() {
+      DiskCleanup::ResetConf();
    }
 };
