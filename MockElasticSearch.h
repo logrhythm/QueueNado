e47f605a26d09c3f0152c9a7baa3bf3cc9a83472
Ben Aldrich <ben.aldrich@logrhythm.com>
2013-07-12 17:42:40 -0600

    remove more overrides that don't work with production builds

--- MockElasticSearch.h
+++ MockElasticSearch.h
@@ -8,7 +8,7 @@ class MockElasticSearch : public ElasticSearch {
 public:
 
    MockElasticSearch() : mMyTransport(""), ElasticSearch(mMyTransport), mFakeIndexList(true),
-   mFakeDeleteIndex(true), mFakeDeleteValue(true){
+   mFakeDeleteIndex(true), mFakeDeleteValue(true) {
       mMockListOfIndexes.insert("kibana-int");
       mMockListOfIndexes.insert("network_1999_01_01");
       mMockListOfIndexes.insert("network_2012_12_31");
@@ -24,37 +24,37 @@ public:
 
    }
 
-   LR_VIRTUAL std::set<std::string> GetListOfIndexeNames() override {
+   LR_VIRTUAL std::set<std::string> GetListOfIndexeNames() {
       if (mFakeIndexList) {
          return mMockListOfIndexes;
       }
       return ElasticSearch::GetListOfIndexeNames();
    }
 
-   LR_VIRTUAL bool CreateIndex(const std::string& indexName, int shards, int replicas)override {
+   LR_VIRTUAL bool CreateIndex(const std::string& indexName, int shards, int replicas) {
       return ElasticSearch::CreateIndex(indexName, shards, replicas);
    }
 
-   LR_VIRTUAL bool DeleteIndex(const std::string& indexName) override {
+   LR_VIRTUAL bool DeleteIndex(const std::string& indexName) {
       if (mFakeDeleteIndex) {
          return mFakeDeleteValue;
       }
       ElasticSearch::DeleteIndex(indexName);
    }
 
-   LR_VIRTUAL bool IndexClose(const std::string& indexName) override {
+   LR_VIRTUAL bool IndexClose(const std::string& indexName) {
       ElasticSearch::IndexClose(indexName);
    }
 
-   LR_VIRTUAL bool IndexOpen(const std::string& indexName) override {
+   LR_VIRTUAL bool IndexOpen(const std::string& indexName) {
       ElasticSearch::IndexOpen(indexName);
    }
 
-   LR_VIRTUAL bool AddDoc(const std::string& indexName, const std::string& indexType, const std::string& id, const std::string& jsonData)override {
+   LR_VIRTUAL bool AddDoc(const std::string& indexName, const std::string& indexType, const std::string& id, const std::string& jsonData) {
       ElasticSearch::AddDoc(indexName, indexType, id, jsonData);
    }
 
-   LR_VIRTUAL bool DeleteDoc(const std::string& indexName, const std::string& indexType, const std::string& id)override {
+   LR_VIRTUAL bool DeleteDoc(const std::string& indexName, const std::string& indexType, const std::string& id) {
       ElasticSearch::DeleteDoc(indexName, indexType, id);
    }
    MockBoomStick mMyTransport;
