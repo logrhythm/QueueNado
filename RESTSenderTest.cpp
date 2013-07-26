709bdb7d0e18220c36ccb9667a59a8c8dc445b5d
Robert Weber <robert.weber@logrhythm.com>
2013-07-26 10:29:13 -0600

    Benchmarks and Elastic search disk info support

---
+++ RESTSenderTest.cpp
@@ -0,0 +1,106 @@
+#include "RESTSenderTest.h"
+#include "MockBoomStick.h"
+#include <algorithm>
+
+TEST_F(RESTSenderTest, GetListOfClusterNames) {
+   DiskInformation info;
+   SingleDiskInfo singleInfo;
+   info["a"] = singleInfo;
+   info["b"] = singleInfo;
+   info["c"] = singleInfo;
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+   RESTSender sender(transport);
+
+   std::vector<std::string> names = sender.GetAllClusterNamesFromDiskInfo(info);
+
+   ASSERT_EQ(3, names.size());
+   std::string findme("a");
+   EXPECT_TRUE(std::find(names.begin(), names.end(), findme) != names.end());
+   findme = "b";
+   EXPECT_TRUE(std::find(names.begin(), names.end(), findme) != names.end());
+   findme = "c";
+   EXPECT_TRUE(std::find(names.begin(), names.end(), findme) != names.end());
+}
+
+TEST_F(RESTSenderTest, GetSingleDiskInfo) {
+   DiskInformation info;
+   SingleDiskInfo singleInfo;
+   std::get<0>(singleInfo)["a"] = "A";
+   std::get<1>(singleInfo)["nA"] = 1;
+   info["a"] = singleInfo;
+   std::get<0>(singleInfo)["a"] = "B";
+   std::get<1>(singleInfo)["nA"] = 2;
+   info["b"] = singleInfo;
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+   RESTSender sender(transport);
+
+   SingleDiskInfo retrievedInfo;
+   ASSERT_TRUE(sender.GetAClustersDiskInfo("b", info,retrievedInfo));
+   EXPECT_EQ(singleInfo, retrievedInfo);
+   std::get<0>(singleInfo)["a"] = "A";
+   std::get<1>(singleInfo)["nA"] = 1;
+   ASSERT_TRUE(sender.GetAClustersDiskInfo("a", info,retrievedInfo));
+   EXPECT_EQ(singleInfo, retrievedInfo);
+   EXPECT_FALSE(sender.GetAClustersDiskInfo("c",info,retrievedInfo));
+
+}
+
+TEST_F(RESTSenderTest, GetSpecificDiskInfo) {
+   DiskInformation info;
+   SingleDiskInfo singleInfo;
+   std::get<0>(singleInfo)["disk_write_size"] = "589.8gb";
+   std::get<0>(singleInfo)["disk_read_size"] = "106.1gb";
+   std::get<0>(singleInfo)["free"] = "21.9gb";
+   std::get<0>(singleInfo)["dev"] = "/dev/mapper/vg_robert2-lv_home";
+   std::get<0>(singleInfo)["available"] = "18.3gb";
+   std::get<0>(singleInfo)["disk_queue"] = "0.2";
+   std::get<0>(singleInfo)["total"] = "72.6gb";
+   std::get<0>(singleInfo)["mount"] = "/home";
+   std::get<0>(singleInfo)["path"] = "/usr/local/probe/db/elasticsearch/data/logrhythm/nodes/0";
+   std::get<1>(singleInfo)["disk_reads"] = 2533055;
+   std::get<1>(singleInfo)["available_in_bytes"] = 19653607424;
+   std::get<1>(singleInfo)["disk_read_size_in_bytes"] = 113932321792;
+   std::get<1>(singleInfo)["disk_write_size_in_bytes"] = 633309597696;
+   std::get<1>(singleInfo)["disk_writes"] = 155087317;
+   std::get<1>(singleInfo)["free_in_bytes"] = 23618060288;
+   std::get<1>(singleInfo)["total_in_bytes"] = 78044545024;
+   std::string clusterName = "a";
+   info[clusterName] = singleInfo;
+
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+   RESTSender sender(transport);
+   EXPECT_EQ("589.8gb", sender.GetDiskWriteSize(clusterName, info));
+   EXPECT_EQ("106.1gb", sender.GetDiskReadSize(clusterName, info));
+   EXPECT_EQ("/dev/mapper/vg_robert2-lv_home", sender.GetDiskDev(clusterName, info));
+   EXPECT_EQ("21.9gb", sender.GetDiskFree(clusterName, info));
+   EXPECT_EQ("18.3gb", sender.GetDiskAvailable(clusterName, info));
+   EXPECT_EQ("0.2", sender.GetDiskQueue(clusterName, info));
+   EXPECT_EQ("72.6gb", sender.GetDiskTotal(clusterName, info));
+   EXPECT_EQ("/home", sender.GetDiskMount(clusterName, info));
+   EXPECT_EQ("/usr/local/probe/db/elasticsearch/data/logrhythm/nodes/0", sender.GetDiskPath(clusterName, info));
+   EXPECT_EQ(2533055, sender.GetDiskReads(clusterName, info));
+   EXPECT_EQ(19653607424, sender.GetDiskAvalInBytes(clusterName, info));
+   EXPECT_EQ(113932321792, sender.GetDiskReadSizeInBytes(clusterName, info));
+   EXPECT_EQ(633309597696, sender.GetDiskWriteSizeInBytes(clusterName, info));
+   EXPECT_EQ(155087317, sender.GetDiskWrites(clusterName, info));
+   EXPECT_EQ(23618060288, sender.GetDiskFreeInBytes(clusterName, info));
+   EXPECT_EQ(78044545024, sender.GetDiskTotalInBytes(clusterName, info));
+   clusterName = "bar";
+   EXPECT_EQ("", sender.GetDiskWriteSize(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskReadSize(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskDev(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskFree(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskAvailable(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskQueue(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskTotal(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskMount(clusterName, info));
+   EXPECT_EQ("", sender.GetDiskPath(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskReads(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskAvalInBytes(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskReadSizeInBytes(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskWriteSizeInBytes(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskWrites(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskFreeInBytes(clusterName, info));
+   EXPECT_EQ(0, sender.GetDiskTotalInBytes(clusterName, info));
+}
+
--- transferZeromqTest.cpp
+++ transferZeromqTest.cpp
@@ -3,105 +3,129 @@
 #include <unordered_set>
 #include <unordered_map>
 #include "ElasticSearch.h"
+#include "MockBoomStick.h"
 #include <thread>
 #include "jansson.h"
 
-void MesageSendThread(int numberOfMessages, int threadNumber, const std::string& binding,
-        const std::string& baseMessage, zctx_t* context) {
-   void* socket = zsocket_new(context, ZMQ_DEALER);
-   std::string resultString;
-   if (zsocket_connect(socket, binding.c_str()) == 0) {
-      std::stringstream testStream;
-      testStream << "tweet" << threadNumber;
-      std::string type = testStream.str();
-      testStream.str("");
-      std::unordered_set<int> ids;
-      int sent = 0;
-      int recieved = 0;
-      for (int j = 0; j < numberOfMessages / 1000; j++) {
-         for (int i = 0; i < 1000 && sent < numberOfMessages; i++) {
+namespace {
 
-            testStream << "POST|/twitter/" << type << "/" << i << "|{\"user\" : \"kimchy\", \"post_date\" : \"2009-11-15T"
-                    << i / (60 * 60) % 24 << ":" << (i / 60) % 60 << ":" << i % 60 << "\", \"message\" : \"" << baseMessage << i
-                    << "\"}";
-            zmsg_t* message = zmsg_new();
-            zmsg_addstr(message, testStream.str().c_str());
-            zmsg_send(&message, socket);
-            sent++;
-            ids.insert(i);
-            testStream.str("");
+   void MessageSend(int numberOfMessages, int threadNumber, const std::string& binding,
+           const std::string& baseMessage, zctx_t* context, size_t& dataSent, size_t& recordsSent) {
+      dataSent = 0;
+      recordsSent = 0;
+      void* socket = zsocket_new(context, ZMQ_DEALER);
+      std::string resultString;
+      if (zsocket_connect(socket, binding.c_str()) == 0) {
+         std::stringstream testStream;
+         testStream << "tweet" << threadNumber;
+         std::string type = testStream.str();
+         testStream.str("");
+         std::unordered_set<int> ids;
+         int sent = 0;
+         int recieved = 0;
+         for (int j = 0; j < numberOfMessages / 1000; j++) {
+            for (int i = 0; i < 1000 && sent < numberOfMessages; i++) {
+
+               testStream << "POST|/twitter/" << type << "/" << i << "|{\"user\" : \"kimchy\", \"post_date\" : \"2009-11-15T"
+                       << i / (60 * 60) % 24 << ":" << (i / 60) % 60 << ":" << i % 60 << "\", \"message\" : \"" << baseMessage << i
+                       << "\"}";
+               dataSent += testStream.str().size();
+               zmsg_t* message = zmsg_new();
+               zmsg_addstr(message, testStream.str().c_str());
+               zmsg_send(&message, socket);
+               sent++;
+               ids.insert(i);
+               testStream.str("");
+            }
+            for (int i = 0; i < 1000 && recieved < numberOfMessages; i++) {
+               zmsg_t* message = zmsg_recv(socket);
+               resultString = zmsg_popstr(message);
+
+               ASSERT_TRUE(resultString.find("\"ok\":true") != std::string::npos);
+               ASSERT_TRUE(resultString.find(type) != std::string::npos);
+               size_t idLocation = resultString.find("id\":\"") + 5;
+               ASSERT_TRUE(idLocation != std::string::npos);
+               int id = atoi(resultString.substr(idLocation, resultString.find("\"", idLocation)).c_str());
+               //std::cout << resultString << std::endl;
+               //std::cout << id << resultString.substr(idLocation,resultString.find("\"",idLocation)) << std::endl;
+               ids.erase(id);
+               zmsg_destroy(&message);
+               recieved++;
+               recordsSent++;
+            }
+            ASSERT_TRUE(ids.empty());
+
          }
-         for (int i = 0; i < 1000 && recieved < numberOfMessages; i++) {
-            zmsg_t* message = zmsg_recv(socket);
-            resultString = zmsg_popstr(message);
 
-            ASSERT_TRUE(resultString.find("\"ok\":true") != std::string::npos);
-            ASSERT_TRUE(resultString.find(type) != std::string::npos);
-            size_t idLocation = resultString.find("id\":\"") + 5;
-            ASSERT_TRUE(idLocation != std::string::npos);
-            int id = atoi(resultString.substr(idLocation, resultString.find("\"", idLocation)).c_str());
-            //std::cout << resultString << std::endl;
-            //std::cout << id << resultString.substr(idLocation,resultString.find("\"",idLocation)) << std::endl;
-            ids.erase(id);
-            zmsg_destroy(&message);
-            recieved++;
-         }
-         ASSERT_TRUE(ids.empty());
-
+      } else {
+         int err = zmq_errno();
+         std::string error(zmq_strerror(err));
+         std::cout << "Could not connect to " << binding << ":" << error << std::endl;
+         zctx_destroy(&context);
+         FAIL();
       }
-
-   } else {
-      int err = zmq_errno();
-      std::string error(zmq_strerror(err));
-      std::cout << "Could not connect to " << binding << ":" << error << std::endl;
-      zctx_destroy(&context);
-      ASSERT_TRUE(false);
    }
-}
 
-void JSonPrettyPrint(json_t* Object, int indent) {
-   const char* key;
-   json_t* value;
+   void MesageSendThread(int numberOfMessages, int threadNumber, const std::string& binding,
+           const std::string& baseMessage, zctx_t* context) {
+      size_t dataSent;
+      size_t recordsSent;
+      MessageSend(numberOfMessages, threadNumber, binding, baseMessage, context, dataSent, recordsSent);
+   }
 
-   json_object_foreach(Object, key, value) {
-      for (int i = 0; i < indent; i++) {
-         std::cout << "   ";
-      }
-      std::cout << key << ":";
-      if (json_is_object(value)) {
-         std::cout << std::endl;
-         JSonPrettyPrint(value, indent + 1);
-      } else if (json_is_string(value)) {
-         std::cout << json_string_value(value);
-      } else if (json_is_integer(value)) {
-         std::cout << json_integer_value(value);
-      } else if (json_is_array(value)) {
-         for (int i = 0; i < json_array_size(value); i++) {
-            std::cout << std::endl;
-            JSonPrettyPrint(json_array_get(value, i), indent + 1);
+   void JSonPrettyPrint(json_t* Object, int indent) {
+      const char* key;
+      json_t* value;
+
+      json_object_foreach(Object, key, value) {
+         for (int i = 0; i < indent; i++) {
+            std::cout << "   ";
          }
+         std::cout << key << ":";
+         if (json_is_object(value)) {
+            std::cout << std::endl;
+            JSonPrettyPrint(value, indent + 1);
+         } else if (json_is_string(value)) {
+            std::cout << json_string_value(value);
+         } else if (json_is_integer(value)) {
+            std::cout << json_integer_value(value);
+         } else if (json_is_array(value)) {
+            for (int i = 0; i < json_array_size(value); i++) {
+               std::cout << std::endl;
+               JSonPrettyPrint(json_array_get(value, i), indent + 1);
+            }
+         }
+         std::cout << std::endl;
       }
-      std::cout << std::endl;
+   }
+
+   DiskInformation GetDiskInfo() {
+      RESTBuilder builder;
+      BoomStick transport("tcp://127.0.0.1:9700");
+      RESTSender sender(transport);
+
+      transport.Initialize();
+      std::string resultString;
+      sender.Send(builder.GetDiskInfo(), resultString);
+
+      return sender.GetDiskInformation(resultString);
+   }
+
+   void PrintDiskInfo() {
+      BoomStick transport("tcp://127.0.0.1:9700");
+      RESTSender sender(transport);
+      auto diskInfo = GetDiskInfo();
+      std::vector<std::string> clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfo);
+      for (auto hostName : clusterNames) {
+         std::cout << hostName << " : " << std::endl;
+         std::cout << sender.GetDiskReads(hostName, diskInfo) << " reads" << std::endl;
+         std::cout << sender.GetDiskWrites(hostName, diskInfo) << " writes" << std::endl;
+      }
    }
 }
 
 TEST_F(transferZeromqTest, FileSystemInfoTest) {
-   RESTBuilder builder;
-   BoomStick transport("tcp://127.0.0.1:9700");
-   RESTSender sender(transport);
-   
-   ASSERT_TRUE(transport.Initialize());
-
-   std::string resultString;
-   sender.Send(builder.GetDiskInfo(), resultString);
-
-   json_error_t error;
-   std::string jsonReturnString = resultString.substr(resultString.find("{"));
-   json_t* decodedMessage = json_loads(jsonReturnString.c_str(), 0, &error);
-   ASSERT_TRUE(decodedMessage != NULL);
-   //JSonPrettyPrint(decodedMessage, 0);
-
-   DiskInformation diskInfo = sender.GetDiskInformation(resultString);
+   PrintDiskInfo();
 }
 
 TEST_F(transferZeromqTest, GetListOfIndexes) {
@@ -145,8 +169,9 @@ TEST_F(transferZeromqTest, GetListOfIndexes) {
 TEST_F(transferZeromqTest, singleIteration) {
    zctx_t* context = zctx_new();
    int iterations = 1;
-
-   MesageSendThread(iterations, 1, "tcp://127.0.0.1:9700", "this is a single thread", context);
+   size_t dataSent;
+   size_t recordsSent;
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", "this is a single thread", context, dataSent, recordsSent);
 
 }
 
@@ -158,6 +183,8 @@ TEST_F(transferZeromqTest, multipleThreads) {
    StartTimedSection();
    std::unordered_map<int, std::thread*> threads;
    for (int i = 0; i < threadcount; i++) {
+      size_t dataSent;
+      size_t recordsSent;
       threads[i] = new std::thread(MesageSendThread, iterations, i, "tcp://127.0.0.1:9700",
               "this is a multiple-thread", context);
    }
@@ -169,11 +196,157 @@ TEST_F(transferZeromqTest, multipleThreads) {
 }
 
 TEST_F(transferZeromqTest, SingleThreadSpeedTest) {
+
+   auto diskInfoPre = GetDiskInfo();
    zctx_t* context = zctx_new();
    int iterations = 50000;
-   SetExpectedTime(iterations, 60, 1, 4000L);
+   size_t dataSent;
+   size_t recordsSent;
+   std::string bigMessage;
+   for (int i = 0; i < 10; i++) {
+      bigMessage += "1234567890";
+   }
+   SetExpectedTime(iterations, bigMessage.size(), 10, 4000L);
    StartTimedSection();
-   MesageSendThread(iterations, 1, "tcp://127.0.0.1:9700", "this is a single thread", context);
+
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", bigMessage, context, dataSent, recordsSent);
    EndTimedSection();
+   std::cout << recordsSent << " total records sent " << std::endl;
+   std::cout << dataSent << " total bytes sent " << std::endl;
+   auto diskInfoPost = GetDiskInfo();
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+   RESTSender sender(transport);
+   std::vector<std::string> clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfoPre);
+   for (auto hostName : clusterNames) {
+      std::cout << hostName << " : " << std::endl;
+      std::cout << (sender.GetDiskReads(hostName, diskInfoPost) - sender.GetDiskReads(hostName, diskInfoPre)) << " reads" << std::endl;
+      std::cout << (sender.GetDiskWrites(hostName, diskInfoPost) - sender.GetDiskWrites(hostName, diskInfoPre)) << " writes" << std::endl;
+      std::cout << (sender.GetDiskReadSizeInBytes(hostName, diskInfoPost) - sender.GetDiskReadSizeInBytes(hostName, diskInfoPre)) << " bytes read" << std::endl;
+      std::cout << (sender.GetDiskWriteSizeInBytes(hostName, diskInfoPost) - sender.GetDiskWriteSizeInBytes(hostName, diskInfoPre)) << " bytes written" << std::endl;
+
+   }
+   iterations = 5000;
+   diskInfoPre = GetDiskInfo();
+   SetExpectedTime(iterations, bigMessage.size(), 10, 4000L);
+   StartTimedSection();
+
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", bigMessage, context, dataSent, recordsSent);
+   EndTimedSection();
+   std::cout << recordsSent << " total records sent " << std::endl;
+   std::cout << dataSent << " total bytes sent " << std::endl;
+   diskInfoPost = GetDiskInfo();
+   clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfoPre);
+   for (auto hostName : clusterNames) {
+      std::cout << hostName << " : " << std::endl;
+      std::cout << (sender.GetDiskReads(hostName, diskInfoPost) - sender.GetDiskReads(hostName, diskInfoPre)) << " reads" << std::endl;
+      std::cout << (sender.GetDiskWrites(hostName, diskInfoPost) - sender.GetDiskWrites(hostName, diskInfoPre)) << " writes" << std::endl;
+      std::cout << (sender.GetDiskReadSizeInBytes(hostName, diskInfoPost) - sender.GetDiskReadSizeInBytes(hostName, diskInfoPre)) << " bytes read" << std::endl;
+      std::cout << (sender.GetDiskWriteSizeInBytes(hostName, diskInfoPost) - sender.GetDiskWriteSizeInBytes(hostName, diskInfoPre)) << " bytes written" << std::endl;
+
+   }
+   
 }
 
+TEST_F(transferZeromqTest, SingleThreadSpeedTestMedium) {
+
+   auto diskInfoPre = GetDiskInfo();
+   zctx_t* context = zctx_new();
+   int iterations = 50000;
+   size_t dataSent;
+   size_t recordsSent;
+   std::string bigMessage;
+   for (int i = 0; i < 100; i++) {
+      bigMessage += "1234567890";
+   }
+   SetExpectedTime(iterations, bigMessage.size(), 10, 4000L);
+   StartTimedSection();
+
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", bigMessage, context, dataSent, recordsSent);
+   EndTimedSection();
+   std::cout << recordsSent << " total records sent " << std::endl;
+   std::cout << dataSent << " total bytes sent " << std::endl;
+   auto diskInfoPost = GetDiskInfo();
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+   RESTSender sender(transport);
+   std::vector<std::string> clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfoPre);
+   for (auto hostName : clusterNames) {
+      std::cout << hostName << " : " << std::endl;
+      std::cout << (sender.GetDiskReads(hostName, diskInfoPost) - sender.GetDiskReads(hostName, diskInfoPre)) << " reads" << std::endl;
+      std::cout << (sender.GetDiskWrites(hostName, diskInfoPost) - sender.GetDiskWrites(hostName, diskInfoPre)) << " writes" << std::endl;
+      std::cout << (sender.GetDiskReadSizeInBytes(hostName, diskInfoPost) - sender.GetDiskReadSizeInBytes(hostName, diskInfoPre)) << " bytes read" << std::endl;
+      std::cout << (sender.GetDiskWriteSizeInBytes(hostName, diskInfoPost) - sender.GetDiskWriteSizeInBytes(hostName, diskInfoPre)) << " bytes written" << std::endl;
+
+   }
+   iterations = 5000;
+   diskInfoPre = GetDiskInfo();
+   SetExpectedTime(iterations, bigMessage.size(), 10, 4000L);
+   StartTimedSection();
+
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", bigMessage, context, dataSent, recordsSent);
+   EndTimedSection();
+   std::cout << recordsSent << " total records sent " << std::endl;
+   std::cout << dataSent << " total bytes sent " << std::endl;
+   diskInfoPost = GetDiskInfo();
+   clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfoPre);
+   for (auto hostName : clusterNames) {
+      std::cout << hostName << " : " << std::endl;
+      std::cout << (sender.GetDiskReads(hostName, diskInfoPost) - sender.GetDiskReads(hostName, diskInfoPre)) << " reads" << std::endl;
+      std::cout << (sender.GetDiskWrites(hostName, diskInfoPost) - sender.GetDiskWrites(hostName, diskInfoPre)) << " writes" << std::endl;
+      std::cout << (sender.GetDiskReadSizeInBytes(hostName, diskInfoPost) - sender.GetDiskReadSizeInBytes(hostName, diskInfoPre)) << " bytes read" << std::endl;
+      std::cout << (sender.GetDiskWriteSizeInBytes(hostName, diskInfoPost) - sender.GetDiskWriteSizeInBytes(hostName, diskInfoPre)) << " bytes written" << std::endl;
+
+   }
+  
+
+}
+
+TEST_F(transferZeromqTest, SingleThreadSpeedTestBigData) {
+
+   auto diskInfoPre = GetDiskInfo();
+   zctx_t* context = zctx_new();
+   int iterations = 50000;
+
+   size_t dataSent;
+   size_t recordsSent;
+   std::string bigMessage;
+   for (int i = 0; i < 1024; i++) {
+      bigMessage += "1234567890";
+   }
+   SetExpectedTime(iterations, bigMessage.size(), 100, 4000L);
+   StartTimedSection();
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", bigMessage, context, dataSent, recordsSent);
+   EndTimedSection();
+   std::cout << recordsSent << " total records sent " << std::endl;
+   std::cout << dataSent << " total bytes sent " << std::endl;
+   auto diskInfoPost = GetDiskInfo();
+   MockBoomStick transport("tcp://127.0.0.1:9700");
+   RESTSender sender(transport);
+   std::vector<std::string> clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfoPre);
+   for (auto hostName : clusterNames) {
+      std::cout << hostName << " : " << std::endl;
+      std::cout << (sender.GetDiskReads(hostName, diskInfoPost) - sender.GetDiskReads(hostName, diskInfoPre)) << " reads" << std::endl;
+      std::cout << (sender.GetDiskWrites(hostName, diskInfoPost) - sender.GetDiskWrites(hostName, diskInfoPre)) << " writes" << std::endl;
+      std::cout << (sender.GetDiskReadSizeInBytes(hostName, diskInfoPost) - sender.GetDiskReadSizeInBytes(hostName, diskInfoPre)) << " bytes read" << std::endl;
+      std::cout << (sender.GetDiskWriteSizeInBytes(hostName, diskInfoPost) - sender.GetDiskWriteSizeInBytes(hostName, diskInfoPre)) << " bytes written" << std::endl;
+
+   }
+   iterations = 5000;
+   diskInfoPre = GetDiskInfo();
+   SetExpectedTime(iterations, bigMessage.size(), 100, 4000L);
+   StartTimedSection();
+   MessageSend(iterations, 1, "tcp://127.0.0.1:9700", bigMessage, context, dataSent, recordsSent);
+   EndTimedSection();
+   std::cout << recordsSent << " total records sent " << std::endl;
+   std::cout << dataSent << " total bytes sent " << std::endl;
+   diskInfoPost = GetDiskInfo();
+   clusterNames = sender.GetAllClusterNamesFromDiskInfo(diskInfoPre);
+   for (auto hostName : clusterNames) {
+      std::cout << hostName << " : " << std::endl;
+      std::cout << (sender.GetDiskReads(hostName, diskInfoPost) - sender.GetDiskReads(hostName, diskInfoPre)) << " reads" << std::endl;
+      std::cout << (sender.GetDiskWrites(hostName, diskInfoPost) - sender.GetDiskWrites(hostName, diskInfoPre)) << " writes" << std::endl;
+      std::cout << (sender.GetDiskReadSizeInBytes(hostName, diskInfoPost) - sender.GetDiskReadSizeInBytes(hostName, diskInfoPre)) << " bytes read" << std::endl;
+      std::cout << (sender.GetDiskWriteSizeInBytes(hostName, diskInfoPost) - sender.GetDiskWriteSizeInBytes(hostName, diskInfoPre)) << " bytes written" << std::endl;
+
+   }
+   
+}
