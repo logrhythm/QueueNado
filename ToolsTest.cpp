ed7ea6eeb6413dc19405959006a3df554415d644
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2013-08-01 17:17:57 -0600

    added failing test for tools

---
+++ ToolsTest.cpp
@@ -0,0 +1,7 @@
+
+
+#include "ToolsTest.h"
+
+TEST_F(ToolsTest, thisShouldFail) {
+   ASSERT_TRUE(false);
+}
---
+++ ToolsTest.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   ToolsTest.h
+ * Author: kjell
+ *
+ * Created on August 1, 2013, 4:57 PM
+ */
+
+#pragma once
+#include "gtest/gtest.h"
+class ToolsTest : public ::testing::Test {
+public:
+
+    ToolsTest() { };
+protected:
+    
+    virtual void SetUp() {     };
+
+    virtual void TearDown() {    };
+private:
+
+};
