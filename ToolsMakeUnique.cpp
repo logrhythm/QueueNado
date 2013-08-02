3704a0c39a4ee7bdbaf02a9cac91fccc426fa48b
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2013-08-02 09:54:03 -0600

    Updated tools test cases

---
+++ ToolsMakeUnique.cpp
@@ -0,0 +1,33 @@
+
+
+#include "ToolsMakeUnique.h"
+#include "MakeUniquePtr.h"
+
+TEST_F(ToolsTest, MakeUniqueSimpleStruct) {
+   bool alive = false;
+   auto simplePtr = std2::make_unique<Simple>(alive);
+   ASSERT_TRUE(alive);
+   simplePtr.reset();
+   ASSERT_FALSE(alive);
+}
+
+TEST_F(ToolsTest, ScopedMakeUniqueSimpleStruct) {
+   bool alive = false;
+   {
+      auto simplePtr = std2::make_unique<Simple>(alive);
+      ASSERT_TRUE(alive);
+   }
+   ASSERT_FALSE(alive);
+}
+
+TEST_F(ToolsTest, ArrayMakeUnique) {
+   bool alive1 = false;
+   bool alive2 = false;
+   {
+      auto simpleArrayPtr = std2::make_unique<Simple[]>(alive1, alive2);
+      ASSERT_TRUE(alive1);
+      ASSERT_TRUE(alive2);
+   }
+   ASSERT_FALSE(alive1);
+   ASSERT_FALSE(alive2);
+}
--- ToolsTest.cpp
+++
@@ -1,7 +0,0 @@
-
-
-#include "ToolsTest.h"
-
-TEST_F(ToolsTest, thisShouldFail) {
-   ASSERT_TRUE(false);
-}
