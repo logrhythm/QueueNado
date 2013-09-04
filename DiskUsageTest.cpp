7fd8864291c82f013471e0360c8e81f64b8245d3
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2013-09-04 16:10:50 -0600

    Updated to be closed to values from df

--- DiskUsageTest.cpp
+++ DiskUsageTest.cpp
@@ -8,12 +8,10 @@
 #include "DiskUsageTest.h"
 #include "DiskUsage.h"
 #include "MockDiskUsage.h"
-
-
-
-TEST(DiskUsage, InstantiationPreDiskReading) {
-   bool mockStats = true;
-   MockDiskUsage usage("/home", mockStats);
+#include <cmath>
+TEST(DiskUsage, FailedReading) {
+   bool doNotMockStats = false;
+   MockDiskUsage usage("abc", doNotMockStats);
    auto bytesUsed = usage.DiskUsed(DiskUsage::Size::Byte);
    auto bytesTotal = usage.DiskTotal(DiskUsage::Size::Byte);
    auto bytesFree = usage.DiskFree(DiskUsage::Size::Byte);
@@ -24,7 +22,7 @@ TEST(DiskUsage, InstantiationPreDiskReading) {
    EXPECT_EQ(usePercentage, 0);
 }
 
-TEST(DiskUsage, InstantiationPostDiskReading) {
+TEST(DiskUsage, ReadAtStartup) {
    bool mockStats = true;
    MockDiskUsage usage("/home", mockStats);
    usage.Update();
@@ -67,20 +65,20 @@ TEST(DiskUsage, ByteToKByteToMBToGB) {
    auto MBTotal = usage.DiskTotal(DiskUsage::Size::MB);
    auto MBFree = usage.DiskFree(DiskUsage::Size::MB);
    auto MBAvailable = usage.DiskAvailable(DiskUsage::Size::MB);
-   EXPECT_EQ(MBUsed, KBUsed/1024);
-   EXPECT_EQ(MBTotal, KBTotal/1024);
-   EXPECT_EQ(MBFree, KBFree/1024);
-   EXPECT_EQ(MBAvailable, KBAvailable/1024);
+   EXPECT_EQ(MBUsed, 1 + KBUsed/1024); // ceiling adds +1
+   EXPECT_EQ(MBTotal, 1 + KBTotal/1024);
+   EXPECT_EQ(MBFree, 1 +KBFree/1024);
+   EXPECT_EQ(MBAvailable, 1 +KBAvailable/1024);
    
    
    auto GBUsed = usage.DiskUsed(DiskUsage::Size::GB);
    auto GBTotal = usage.DiskTotal(DiskUsage::Size::GB);
    auto GBFree = usage.DiskFree(DiskUsage::Size::GB);
    auto GBAvailable = usage.DiskAvailable(DiskUsage::Size::GB);
-   EXPECT_EQ(GBUsed, MBUsed/1024);
-   EXPECT_EQ(GBTotal, MBTotal/1024);
-   EXPECT_EQ(GBFree, MBFree/1024);
-   EXPECT_EQ(GBAvailable, MBAvailable/1024);
+   EXPECT_EQ(GBUsed, 1 + MBUsed/1024);
+   EXPECT_EQ(GBTotal, 1 + MBTotal/1024);
+   EXPECT_EQ(GBFree, 1 + MBFree/1024);
+   EXPECT_EQ(GBAvailable, 1 + MBAvailable/1024);
 }
 
 
@@ -92,4 +90,40 @@ TEST(DiskUsage, PercentageUsed) {
    double usePercentage = usage.DiskUsedPercentage();
    EXPECT_TRUE(usePercentage > 18) << ". Real percentage was: " << usePercentage;
    EXPECT_TRUE(usePercentage < 19) << ". Real percentage was: " << usePercentage;
+}
+
+
+TEST(DiskUsage, CheckValuesMB) {
+   bool mockStats = true;
+   MockDiskUsage usage("/home", mockStats);
+   auto used = usage.DiskUsed(DiskUsage::Size::MB);
+   auto total = usage.DiskTotal(DiskUsage::Size::MB);
+   auto free = usage.DiskFree(DiskUsage::Size::MB);
+   auto available = usage.DiskAvailable(DiskUsage::Size::MB);
+   auto percentage = usage.DiskUsedPercentage();
+
+   // from snapshot 
+   EXPECT_EQ(used, 13279);
+   EXPECT_EQ(total, 74422);
+   EXPECT_EQ(free, 61143); // free is available + "overhead"
+   EXPECT_EQ(available, 57362);
+   EXPECT_EQ(std::ceil(percentage), std::ceil(18.80));
+}
+
+
+TEST(DiskUsage, CheckValuesGB) {
+   bool mockStats = true;
+   MockDiskUsage usage("/home", mockStats);
+   auto used = usage.DiskUsed(DiskUsage::Size::GB);
+   auto total = usage.DiskTotal(DiskUsage::Size::GB);
+   auto free = usage.DiskFree(DiskUsage::Size::GB);
+   auto available = usage.DiskAvailable(DiskUsage::Size::GB);
+   auto percentage = usage.DiskUsedPercentage();
+
+   // from snapshot 
+   EXPECT_EQ(used, 13);
+   EXPECT_EQ(total, 73);
+   EXPECT_EQ(free, 60);
+   EXPECT_EQ(available, 57);
+   EXPECT_EQ(std::ceil(percentage), std::ceil(18.79));
 }
