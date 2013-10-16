1b38a6124e847079333fc2e30c5ab8258ce7798b
John Gress <john.gress@logrhythm.com>
2013-10-16 10:37:41 -0600

    Add Syslog_Send stat

--- MockSyslogReporter.h
+++ MockSyslogReporter.h
@@ -13,7 +13,10 @@ class MockSyslogReporter : public SyslogReporter {
       MockSyslogReporter(ConfSlave& slave, const std::string& name,
               const int option, const int facility, const int priority)
          : SyslogReporter(slave, name, option, facility, priority),
-           mSyslogEnabled(true) {};
+           mSyslogEnabled(true),
+           mTvSec(0),
+           mTvUSec(0),
+           mStatCount(0) {};
 
       boost::thread* Start() {
          return SyslogReporter::Start();
@@ -31,8 +34,37 @@ class MockSyslogReporter : public SyslogReporter {
          mSyslogEnabled = val;
       }
 
+      void SendAccumStat(unsigned int stat) {
+         mStatCount = stat;
+      }
+
+      unsigned int GetStatCount() {
+         return mStatCount;
+      }
+
+      void ResetStatCount() {
+         mStatCount = 0;
+      }
+
+      void GetTime(struct timeval& tv) {
+         if ( mTvSec != 0 ) {
+            tv.tv_sec = mTvSec;
+            tv.tv_usec = mTvUSec;
+         } else {
+            SyslogReporter::GetTime(tv);
+         }
+      }
+
+      void SetMockTime(time_t tvSec, time_t tvUSec) {
+         mTvSec = tvSec;
+         mTvUSec = tvUSec;
+      }
+
    private:
       bool mSyslogEnabled;
+      time_t mTvSec;
+      time_t mTvUSec;
+      unsigned int mStatCount;
 
 };
 
--- SyslogReporterTest.cpp
+++ SyslogReporterTest.cpp
@@ -23,7 +23,6 @@ void SyslogReporterTest::ShootZeroCopySyslogThread(int numberOfMessages,
    rifle.SetIOThreads(1);
    rifle.SetOwnSocket(false);
    rifle.Aim();
-   unsigned int hash(0);
    std::string* syslogMsg = new std::string(exampleSyslog);
    for (int i = 0; i < numberOfMessages && !zctx_interrupted; i++) {
       std::string* syslogMsg = new std::string(exampleSyslog);
@@ -131,4 +130,65 @@ TEST_F(SyslogReporterTest, SendMsgToSyslogReporterSyslogDisabled) {
 
    EXPECT_EQ(0, syslogOutput.size());
    syslogReporter.Join();
+}
+
+TEST_F(SyslogReporterTest, SyslogSendStat) {
+   MockSyslogReporter syslogReporter(mConfSlave, syslogName, syslogOption, 
+         syslogFacility, syslogPriority);
+   syslogReporter.SetMockTime( 10, 0 );
+   boost::thread* srThread = syslogReporter.Start(); 
+   boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+   EXPECT_NE(syslogReporter.GetThreadId(), 0);
+
+   std::string queueName = mConfSlave.GetConf().getSyslogQueue();
+   int numSyslogMsgs = 10;
+
+   Rifle rifle(queueName);
+   rifle.SetHighWater(500);
+   rifle.SetIOThreads(1);
+   rifle.SetOwnSocket(false);
+   rifle.Aim();
+
+   std::string* pMsg = NULL;
+   std::string syslogMsg(" EVT:001 49f7c7f9-73c1-4c6f-9585-fca792087297:00 10.128.20.60,10.128.64.251,58177,53,f0:1f:af:0b:dc:2d,f0:f7:55:dc:a8:7f,17,793,98/98,172/172,1/1,1381436188,1381436247,59/59,process=dns");
+   for (int i = 0; i < numSyslogMsgs && !zctx_interrupted; i++) {
+      pMsg = new std::string(syslogMsg);
+      ASSERT_TRUE(rifle.FireZeroCopy(pMsg, pMsg->size(), ZeroCopyDelete, 10));
+   }
+   boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+   EXPECT_EQ(0, syslogReporter.GetStatCount());
+   syslogReporter.SetMockTime( 15, 0 );
+   boost::this_thread::sleep(boost::posix_time::milliseconds(600));
+   EXPECT_EQ(numSyslogMsgs, syslogReporter.GetStatCount());
+   
+   syslogReporter.SetMockTime( 16, 0 );
+   boost::this_thread::sleep(boost::posix_time::milliseconds(600));
+   EXPECT_EQ(0, syslogReporter.GetStatCount());
+
+   numSyslogMsgs = 5;
+   for (int i = 0; i < numSyslogMsgs && !zctx_interrupted; i++) {
+      pMsg = new std::string(syslogMsg);
+      ASSERT_TRUE(rifle.FireZeroCopy(pMsg, pMsg->size(), ZeroCopyDelete, 10));
+   }
+   boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+   EXPECT_EQ(0, syslogReporter.GetStatCount());
+   syslogReporter.SetMockTime( 17, 0 );
+   boost::this_thread::sleep(boost::posix_time::milliseconds(600));
+   EXPECT_EQ(numSyslogMsgs, syslogReporter.GetStatCount());
+
+   syslogReporter.Join();
+}
+
+TEST_F(SyslogReporterTest, SendStatTime) {
+   MockSyslogReporter syslogReporter(mConfSlave, syslogName, syslogOption, 
+         syslogFacility, syslogPriority);
+
+   syslogReporter.SetMockTime( 1, 0 );
+   EXPECT_TRUE(syslogReporter.IsSendStatTime());
+   EXPECT_FALSE(syslogReporter.IsSendStatTime());
+   syslogReporter.SetMockTime( 1, 999999 );
+   EXPECT_FALSE(syslogReporter.IsSendStatTime());
+   syslogReporter.SetMockTime( 2, 0 );
+   EXPECT_TRUE(syslogReporter.IsSendStatTime());
+
 }
