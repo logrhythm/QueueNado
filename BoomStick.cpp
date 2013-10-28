1cace2eb841be059f016a85099f616fed675b119
Robert Weber <robert.weber@logrhythm.com>
2013-10-28 14:36:04 -0600

    Another layer of refactor to clean up the tests

--- BoomStick.cpp
+++ BoomStick.cpp
@@ -324,7 +324,7 @@ bool BoomStick::SendAsync(const std::string& uuid, const std::string& command) {
          }
       } else {
          LOG(WARNING) << "Queue error, timeout waiting for queue to be ready";
-         std::this_thread::sleep_for(std::chrono::milliseconds(10));
+         std::this_thread::sleep_for(std::chrono::milliseconds(100)); // do not make this any faster!
          success = false;
       }
    }
