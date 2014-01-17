30e87805d053777d3cb507de955e9451b69fb8bc
Robert Weber <robert.weber@logrhythm.com>
2014-01-17 14:34:01 -0700

    More tests!!!

--- ForkerPipeTest.cpp
+++ ForkerPipeTest.cpp
@@ -22,6 +22,78 @@ TEST_F(ForkerPipeTest, Constructors) {
    EXPECT_FALSE(FileIO::DoesFileExist("/tmp/ForkerPipeTest.serverToClient.fifo"));
 }
 #ifdef LR_DEBUG
+
+
+//LR_VIRTUAL bool GetStringFromPipeWithWait(std::string& resultString,const int waitInSeconds) {
+
+//   LR_VIRTUAL bool GetStringFromUniquePipeWithWait(const CommandId& id,std::string& resultString,const int waitInSeconds){
+
+TEST_F(ForkerPipeTest, MakeDestoryUniqueFifos) {
+   MockForkerPipe serverPipe("ForkerPipeTest", false);
+   MockForkerPipe clientPipe("ForkerPipeTest");
+   EXPECT_FALSE(FileIO::DoesFileExist("/tmp/ForkerPipeTest.abc"));
+   EXPECT_FALSE(clientPipe.DestroyUniqueFifo("abc"));
+   ASSERT_TRUE(clientPipe.MakeUniqueFifo("abc"));
+   EXPECT_TRUE(FileIO::DoesFileExist("/tmp/ForkerPipeTest.abc"));
+   EXPECT_FALSE(clientPipe.MakeUniqueFifo("abc"));
+   EXPECT_TRUE(clientPipe.DestroyUniqueFifo("abc"));
+   EXPECT_FALSE(FileIO::DoesFileExist("/tmp/ForkerPipeTest.abc"));
+}
+
+//   LR_VIRTUAL int WriteOpenUniqueFifo(const CommandId& id) {
+//   LR_VIRTUAL std::string GetTargetReceivePipe(){
+//   LR_VIRTUAL std::string GetTargetSendPipe(){
+//   LR_VIRTUAL std::string GetUUID(){
+//   LR_VIRTUAL int RunExecVE(const char* command, char** arguments, char** environment){
+//   LR_VIRTUAL bool IsChildLiving ( pid_t child ){
+
+TEST_F(ForkerPipeTest, ConstructUniquePipeName) {
+   MockForkerPipe serverPipe("ForkerPipeTest", false);
+   MockForkerPipe clientPipe("ForkerPipeTest");
+   
+   std::string name = serverPipe.ConstructUniquePipeName("");
+   EXPECT_EQ("/tmp/ForkerPipeTest",name);
+   std::string name = serverPipe.ConstructUniquePipeName("fubar");
+   EXPECT_EQ("/tmp/ForkerPipeTest.fubar",name);
+}
+TEST_F(ForkerPipeTest, WaitForDataOnPipe) {
+   MockForkerPipe serverPipe("ForkerPipeTest", false);
+   MockForkerPipe clientPipe("ForkerPipeTest");
+   ASSERT_TRUE(clientPipe.MakeUniqueFifo("abc"));
+   EXPECT_FALSE(serverPipe.WaitForDataOnPipe(0,1));
+   std::thread clientThread([](MockForkerPipe& clientPipe) {    
+      clientPipe.SendStringToUniquePipe("abc","def",30);
+   }
+   , std::ref(clientPipe));
+   
+   int pipe = serverPipe.ReadOpenUniqueFifo("abc");
+   ASSERT_TRUE(serverPipe.WaitForDataOnPipe(pipe,10));
+   std::string result = serverPipe.ReadFromReadyPipe(pipe);
+   close(pipe);
+   clientThread.join();
+   EXPECT_EQ("def",result);
+   EXPECT_TRUE(clientPipe.DestroyUniqueFifo("abc"));
+}
+
+TEST_F(ForkerPipeTest, ParseCommandProto) { 
+   std::string command;
+   std::vector<std::string> args;
+   protoMsg::ForkerRequest requestProto;
+   MockForkerPipe testPipe("ForkerPipeTest",false);
+   
+   EXPECT_FALSE(testPipe.ParseCommandProto(requestProto,command,args));
+   requestProto.set_command("test");
+   EXPECT_TRUE(testPipe.ParseCommandProto(requestProto,command,args));
+   EXPECT_EQ("test",command);
+   requestProto.add_args("arg1");
+   requestProto.add_args("arg2");
+   EXPECT_TRUE(testPipe.ParseCommandProto(requestProto,command,args));
+   EXPECT_EQ("test",command);
+   ASSERT_EQ(2,args.size());
+   EXPECT_EQ("arg1",args[0]);
+   EXPECT_EQ("arg2",args[1]);
+   
+}
 TEST_F(ForkerPipeTest, GetResultOfSentCommand) {
    MockForkerPipe serverPipe("ForkerPipeTest", false);
    MockForkerPipe clientPipe("ForkerPipeTest");
--- MockForkerPipe.h
+++ MockForkerPipe.h
@@ -8,4 +8,48 @@ public:
    LR_VIRTUAL int GetReplyTimeout() { return 1;}
    LR_VIRTUAL int GetSendReplyTimeout(){ return 1;}
    LR_VIRTUAL int GetRunningCommandTimeout(){ return 1;}
+   LR_VIRTUAL bool GetStringFromPipeWithWait(std::string& resultString,const int waitInSeconds) {
+      return ForkerPipe::GetStringFromPipeWithWait(resultString,waitInSeconds);
+   }
+   LR_VIRTUAL bool GetStringFromUniquePipeWithWait(const CommandId& id,std::string& resultString,const int waitInSeconds){
+      return ForkerPipe::GetStringFromUniquePipeWithWait(id,resultString,waitInSeconds);
+   }
+   LR_VIRTUAL bool MakeUniqueFifo(const CommandId& id) {
+      return ForkerPipe::MakeUniqueFifo(id);
+   }
+   LR_VIRTUAL bool DestroyUniqueFifo(const CommandId& id) {
+      return ForkerPipe::DestroyUniqueFifo(id);
+   }
+   LR_VIRTUAL int ReadOpenUniqueFifo(const CommandId& id) {
+      return ForkerPipe::ReadOpenUniqueFifo(id);
+   }
+   LR_VIRTUAL int WriteOpenUniqueFifo(const CommandId& id) {
+      return ForkerPipe::WriteOpenUniqueFifo( id);
+   }
+   LR_VIRTUAL std::string ConstructUniquePipeName(const CommandId& id){
+      return ForkerPipe::ConstructUniquePipeName(id);
+   }
+   
+   LR_VIRTUAL std::string GetTargetReceivePipe(){
+      return ForkerPipe::GetTargetReceivePipe();
+   }
+   LR_VIRTUAL std::string GetTargetSendPipe(){
+      return ForkerPipe::GetTargetSendPipe();
+   }
+   LR_VIRTUAL std::string GetUUID(){
+      return ForkerPipe::GetUUID();
+   }
+   LR_VIRTUAL int RunExecVE(const char* command, char** arguments, char** environment){
+      return ForkerPipe::RunExecVE(command, arguments, environment);
+   }
+   LR_VIRTUAL bool IsChildLiving ( pid_t child ){
+      return ForkerPipe::IsChildLiving (child );
+   }
+   LR_VIRTUAL std::string ReadFromReadyPipe(int pipe){
+      return ForkerPipe::ReadFromReadyPipe(pipe);
+   }
+   LR_VIRTUAL bool WaitForDataOnPipe(int pipe, const int waitInSeconds){
+      return ForkerPipe::WaitForDataOnPipe(pipe, waitInSeconds);
+   }
+
 };
