f30f21a72059cc8f85dd7ef66178180d28a66679
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2014-02-25 16:12:37 -0700

    bond interfaces will be considered valid, even if they are not yet created

--- EthInfoTest.cpp
+++ EthInfoTest.cpp
@@ -25,7 +25,7 @@ TEST_F(EthInfoTest, GetIFAddrs) {
    EXPECT_FALSE(ethInfo.IsValid());
    EXPECT_TRUE(ethInfo.GetAvaliableInterfaces().empty());
    ethInfo.mGetIFAddrsRetVal = 0;
-   EXPECT_FALSE(ethInfo.Initialize());
+   EXPECT_FALSE(ethInfo.Initialize()); // aborts before bond0 is added
    EXPECT_FALSE(ethInfo.IsValid());
    EXPECT_TRUE(ethInfo.GetAvaliableInterfaces().empty());  
    ethInfo.mifaddr = reinterpret_cast<struct ifaddrs*>(malloc(sizeof(struct ifaddrs)));
@@ -36,8 +36,8 @@ TEST_F(EthInfoTest, GetIFAddrs) {
    strncpy(ethInfo.mifaddr->ifa_name,fakeName.c_str(),fakeName.size());
    EXPECT_TRUE(ethInfo.Initialize());
    EXPECT_TRUE(ethInfo.IsValid());
-   EXPECT_EQ(1,ethInfo.GetAvaliableInterfaces().size());
-   EXPECT_EQ("eth1",*(ethInfo.GetAvaliableInterfaces().begin()));
+   EXPECT_EQ(2,ethInfo.GetAvaliableInterfaces().size());
+   EXPECT_TRUE(ethInfo.GetAvaliableInterfaces().count("eth1") != 0);
    ethInfo.mifaddr->ifa_next = reinterpret_cast<struct ifaddrs*>(malloc(sizeof(struct ifaddrs)));
    ASSERT_FALSE(NULL == ethInfo.mifaddr->ifa_next);
    ethInfo.mifaddr->ifa_next->ifa_next = NULL;
@@ -47,12 +47,50 @@ TEST_F(EthInfoTest, GetIFAddrs) {
    ASSERT_FALSE(NULL == ethInfo.mifaddr->ifa_name);
    strncpy(ethInfo.mifaddr->ifa_next->ifa_name,fakeName2.c_str(),fakeName2.size());
    EXPECT_TRUE(ethInfo.Initialize());
-   EXPECT_EQ(2,ethInfo.GetAvaliableInterfaces().size());
+   EXPECT_EQ(3,ethInfo.GetAvaliableInterfaces().size());
    free(ethInfo.mifaddr->ifa_next->ifa_name);
    free(ethInfo.mifaddr->ifa_name);
    free(ethInfo.mifaddr->ifa_next);
    free(ethInfo.mifaddr);
 }
+
+TEST_F(EthInfoTest, GetIFAddrsBond0) {
+   MockEthInfo ethInfo;
+   ethInfo.mFakeGetIFAddrs = true;
+   EXPECT_FALSE(ethInfo.Initialize());
+   EXPECT_FALSE(ethInfo.IsValid());
+   EXPECT_TRUE(ethInfo.GetAvaliableInterfaces().empty());
+   ethInfo.mGetIFAddrsRetVal = 0;
+   EXPECT_FALSE(ethInfo.Initialize());
+   EXPECT_FALSE(ethInfo.IsValid());
+   EXPECT_TRUE(ethInfo.GetAvaliableInterfaces().empty());  
+   ethInfo.mifaddr = reinterpret_cast<struct ifaddrs*>(malloc(sizeof(struct ifaddrs)));
+   ethInfo.mifaddr->ifa_next = NULL;
+   
+   std::string fakeName("bond0");
+   ethInfo.mifaddr->ifa_name = reinterpret_cast<char*>(malloc(fakeName.size()));
+   strncpy(ethInfo.mifaddr->ifa_name,fakeName.c_str(),fakeName.size());
+   EXPECT_TRUE(ethInfo.Initialize());
+   EXPECT_TRUE(ethInfo.IsValid());
+   EXPECT_EQ(1,ethInfo.GetAvaliableInterfaces().size());
+   EXPECT_EQ("bond0",*(ethInfo.GetAvaliableInterfaces().begin()));
+  
+   free(ethInfo.mifaddr->ifa_name);
+   free(ethInfo.mifaddr);
+}
+
+
+// It will ALWAYS have at least bond0
+TEST_F(EthInfoTest, SystemGetIFAddrsBond0) {
+   EthInfo ethInfo;
+   EXPECT_TRUE(ethInfo.Initialize());
+   EXPECT_TRUE(ethInfo.IsValid());
+   EXPECT_FALSE(ethInfo.GetAvaliableInterfaces().empty());
+ 
+   std::string bondName("bond0");
+   EXPECT_TRUE(ethInfo.GetAvaliableInterfaces().count(bondName) != 0);
+   }
+
 
 #else 
 TEST_F(EthInfoTest, NullTest) {
