e22323521038e07666d0c3473c835c5b2a14cbe4
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2014-04-15 11:37:24 -0600

    left out a test file

---
+++ ToolsTestFileIO.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "gtest/gtest.h"
+#include <cstdlib>
+#include <string>
+#include "FileIO.h"
+#include <iostream>
+class TestFileIO : public ::testing::Test {
+public:
+
+   TestFileIO() : mTestDirectory("/tmp/TempDirectoryTestOfFileIO") {}
+   virtual ~TestFileIO() = default;
+
+   virtual std::string CreateSubDirectory(const std::string& directory) {
+      std::string mkTestSubDir{"mkdir -p " + mTestDirectory + "/" + directory};
+      if (0 != system(mkTestSubDir.c_str())) {
+         return {};
+      }
+      return {mTestDirectory + "/" + directory};
+   }
+
+   virtual std::string CreateFile(const std::string& directory, const std::string& filename) {
+      std::string mkTestSubDir{"touch " + directory + "/" + filename};
+      if (0 != system(mkTestSubDir.c_str())) {
+         return {};
+      }
+      return {directory + "/" + filename};
+   }
+    
+
+protected:
+
+   virtual void SetUp() {
+      std::string mkTestdir{"mkdir -p " + mTestDirectory};
+      EXPECT_EQ(0, system(mkTestdir.c_str()));
+   }
+
+   virtual void TearDown() {
+      std::string removeDir{"rm -rf " + mTestDirectory};
+      EXPECT_EQ(0, system(removeDir.c_str()));
+   }
+
+   const std::string mTestDirectory;
+};
