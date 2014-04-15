cc314ba679d80e32a620dc4230910522d43ecbfe
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2014-04-15 14:06:18 -0600

    Fixed merged conflicts that were overlooked

--- ToolsTestFileIO.cpp
+++ ToolsTestFileIO.cpp
@@ -119,17 +119,6 @@ TEST_F(TestFileIO, DirectoryExistance) {
    EXPECT_FALSE(FileIO::DoesDirectoryExist(directory));
 }
 
-TEST_F(TestFileIO, DirectoryReader_NotExistingDirectory) {
-   FileIO::DirectoryReader reader{mTestDirectory + "/_#Does_not+_exist"};
-   EXPECT_TRUE(reader.Valid().HasFailed()) << reader.Valid().error;
-}
-
-TEST_F(TestFileIO, DirectoryReader_ExistingDirectory) {
-   FileIO::DirectoryReader reader{mTestDirectory};
-   EXPECT_FALSE(reader.Valid().HasFailed()) << reader.Valid().error;
-}
-
-
 
 TEST_F(TestFileIO,  RemoveEmptyDirectories_FakeDirectoriesAreIgnored) {
    EXPECT_TRUE(FileIO::RemoveEmptyDirectories({}).result); // invalids are ignored
@@ -178,84 +167,6 @@ TEST_F(TestFileIO, CleanDirectoryOfFileContents) {
 
 
 
-
-
-
-
-
-
-
-
-// An empty directory will only contain "." and ".." which we ignores
-TEST_F(TestFileIO, DirectoryReader_NoFilesInDirectory) {
-   FileIO::DirectoryReader reader{mTestDirectory};
-   EXPECT_FALSE(reader.Valid().HasFailed());
-
-   FileIO::DirectoryReader::Entry fileAndType = reader.Next();
-   EXPECT_EQ(fileAndType.first, FileIO::FileType::End);
-   EXPECT_EQ(fileAndType.second, "");
-}
-
-TEST_F(TestFileIO, DirectoryReader_HasFilesInDirectory__AfterReset) {
-   using namespace FileIO;
-
-   DirectoryReader reader{mTestDirectory};
-   DirectoryReader::Entry fileAndType = reader.Next();
-
-   EXPECT_EQ(fileAndType.first, FileType::End);
-   EXPECT_EQ(fileAndType.second, "");
-
-   // We have already reached the end. This must be reset before reading successfully
-   CreateFile(mTestDirectory, "some_file");
-   fileAndType = reader.Next();
-   EXPECT_EQ(fileAndType.first, FileType::End);
-   EXPECT_EQ(fileAndType.second, "");
-
-   // After the reset we can find the file
-   reader.Reset();
-   fileAndType = reader.Next();
-   EXPECT_EQ(fileAndType.first, FileType::File);
-   EXPECT_EQ(fileAndType.second, "some_file");
-
-
-   // has reached the end again
-   fileAndType = reader.Next();
-   EXPECT_EQ(fileAndType.second, "");
-   EXPECT_EQ(fileAndType.first, FileType::End);
-
-
-   CreateSubDirectory("some_directory");
-   EXPECT_TRUE(FileIO::DoesDirectoryExist({mTestDirectory + "/some_directory"}));
-   reader.Reset();
-
-   fileAndType = reader.Next();
-   EXPECT_NE(fileAndType.first, FileType::End);
-
-   std::string filename;
-   std::string directoryname;
-
-   for (size_t count = 0; count < 2; ++count) {
-      if (fileAndType.first == FileType::Directory) {
-         directoryname = fileAndType.second;
-         fileAndType = reader.Next();
-      }
-
-      if (fileAndType.first == FileType::File) {
-         filename = fileAndType.second;
-         reader.Next();
-      }
-   }
-
-   EXPECT_EQ(filename, "some_file");
-   EXPECT_EQ(directoryname, "some_directory");
-   fileAndType = reader.Next();
-
-   EXPECT_EQ(fileAndType.first, FileType::End);
-   EXPECT_EQ(fileAndType.second, "");
-}
-
-
-
 TEST_F(TestFileIO, AThousandFiles) {
    using namespace FileIO;
    for (size_t index = 0; index < 1000; ++index) {
@@ -284,47 +195,6 @@ TEST_F(TestFileIO, AThousandFiles) {
    for (size_t index = 0; index < 1000; ++index) {
       EXPECT_EQ(files[index], std::to_string(index));
    }
-}
-
-
-// FileIO #files   time
-//        63,8841  761 ms
-//        994,080  1 sec
-
-// Boost #files   time
-//       63,8841  3199 ms
-//       985,524  5 sec
-TEST_F(TestFileIO, DISABLED_System_Performance_FileIO__vs_Boost) {
-   using namespace FileIO;
-
-   DirectoryReader::Entry entry;
-
-   StopWatch timeToFind;
-
-   size_t filecounter = 0;
-
-   std::string path = {"/usr/local/probe/pcap"};
-   DirectoryReader reader(path);
-   if (false == reader.Valid().HasFailed()) {
-      reader.Next();
-      while (entry.first != FileIO::FileType::End) {
-         ++filecounter;
-      }
-      entry = reader.Next();
-   }
-
-  
-   LOG(INFO) << "FileIO Time to find " << filecounter << "took: " << timeToFind.ElapsedSec() << " sec";
-   timeToFind.Restart();
-   boost::filesystem::path boostPath = path;
-   boost::filesystem::directory_iterator end;
-   filecounter = 0;
-   for (boost::filesystem::directory_iterator dir_iter(boostPath); dir_iter != end; ++dir_iter) {
-      if (boost::filesystem::is_regular_file(dir_iter->status())) {
-         ++filecounter;
-      }
-   }
-   LOG(INFO)<< "Boost Time to find " << filecounter << "took: " << timeToFind.ElapsedSec() << " sec";
 }
 
 
@@ -408,40 +278,6 @@ TEST_F(TestFileIO, DirectoryReader_HasFilesInDirectory__AfterReset) {
    EXPECT_EQ(fileAndType.second, "");
 }
 
-   //   for (size_t index = 0; index < 1000; ++index) {
-   //  CreateSubDirectory(std::to_string(index));      
-  // }
-TEST_F(TestFileIO, AThousandFiles) {
-   using namespace FileIO;  
-     
-   for (size_t index = 0; index < 1000; ++index) {
-      CreateFile(mTestDirectory, std::to_string(index));
-   }
-   
-   
-   std::vector<std::string> files;
-   DirectoryReader::Entry entry;
-   
-   DirectoryReader reader(mTestDirectory);  
-   StopWatch timeToFind;
-   entry = reader.Next();
-   while(entry.first != FileType::End) {
-      ASSERT_NE(entry.first, FileType::Directory);
-      ASSERT_NE(entry.first, FileType::Unknown);
-      files.push_back(entry.second);
-      entry = reader.Next();
-   }
-   
-   ASSERT_EQ(files.size(), 1000);
-   std::cout << "Time to find 1000 files and save them took: " << timeToFind.ElapsedUs() << " us" << std::endl;
-
-   std::sort(files.begin(), files.end(), [](const std::string& lh, const std::string& rh){
-      return std::stoul(lh) < std::stoul(rh);
-   });
-   for(size_t index = 0; index < 1000; ++index) {
-      EXPECT_EQ(files[index], std::to_string(index));
-   }
-}
 
 TEST_F(TestFileIO, DISABLED_System_Performance_FileIO__vs_Boost) {
    using namespace FileIO;
