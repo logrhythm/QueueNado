fa11eeb0189e2d48706f41586cb32acdc2005d8f
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2014-04-24 13:41:23 -0600

    Added GLIBC sanity test

--- SystemSanityTest.cpp
+++ SystemSanityTest.cpp
@@ -12,88 +12,93 @@
 #include <thread>
 #include <sys/stat.h> // mkdir
 #include <cstdio>     // remove
+
 TEST_F(SystemSanityTest, CompilerCheck_Assert_System_Expectations) {
    static_assert(1 == alignof(char), "Error: This should never fail. What is going on? Alignment of char should ALWAYS be 1");
-   static_assert(8 == sizeof(size_t), "Error: 64-bit support failed. For improved architecture this check can be sizeof(size_t) >= 8");
-   EXPECT_TRUE(true);   
+   static_assert(8 == sizeof (size_t), "Error: 64-bit support failed. For improved architecture this check can be sizeof(size_t) >= 8");
+   EXPECT_TRUE(true);
 }
 
 
+//
+// The purpose of this test is to verify if various GLIBC functions are thread safe when we 
+// are using full path, and not relative path. 
+//
+// For relative path we likely have an issue but we should never use relative path!
+// Bug details:
+// ============
+// Internally the GLIBC functions (chmod, remove, chown etc) will use fts_open. 
+// If fts_open is not called with a specific flag "FTS_NOCHDIR" then it is thread
+// unsafe as another thread using fts_open can cause a change of the active directory. 
+//
+//
+TEST_F(SystemSanityTest, GlibCallsUsingFullPath__ShouldNotAffectOtherDirectories) {
 
-TEST_F(SystemSanityTest, GlibC_FullPath__ShouldNotAffectOtherDirectories) {
-   
-   std:;string dirRemove = CreateSubDirectory("to_remove");
-   std::string dirChange = CreateSubDirectory("to_change");
-      
-   const size_t maxFiles = 10000;
+   std::string dirRemove = CreateSubDirectory("to_remove");
+   std::string dirChange1 = CreateSubDirectory("to_change1");
+   std::string dirChange2 = CreateSubDirectory("to_change2");
+
+   const size_t maxFiles = 1000;
    for (size_t index = 0; index < maxFiles; ++index) {
       CreateFile(dirRemove, std::to_string(index));
-      CreateFile(dirChange, std::to_string(index));
-   }
-   
-   
-   std::vector<std::future<void>> finished;
-   auto changeChmod = [](const std::string& path) {
-      
-      
-   }
-   
-    
-   
-   
-
-   
-      auto dir1 = CreateSubDirectory(std::to_string(dirIndex));
-      CreateFile(dir1, {"f_" + std::to_string(1)});
-      CreateFile(dir1, {"f_" + std::to_string(2)});
-   
-      auto dir2 = CreateSubDirectory({"/to_remove/" + std::to_string(dirIndex)});
-      CreateFile(dir2, {"f_" + std::to_string(1)});
-      CreateFile(dir2, {"f_" + std::to_string(2)});
-   }
-   
-   
-   auto createDir = [](const std::string& fullPathName) -> bool {
-      return (0 == mkdir(fullPathName.c_str(), 777));
+      CreateFile(dirChange1, std::to_string(index));
+      CreateFile(dirChange2, std::to_string(index));
    }
 
-   // will call unlink for file and rmdir for directories
-   auto deleteEntity = [](const std::string& fullPathName) -> bool {
-      return (0 == remove(fullPathName.c_str()));
+
+   std::vector < std::future<bool>> finished;
+   
+   // lambda function for changing permissions on files
+   auto changeChmod = [&maxFiles](const std::string& path, mode_t mode) ->bool {
+      bool success = true;
+      for (size_t index = 0; index < maxFiles; ++index) {
+         std::string filepath = {path + "/" + std::to_string(index)};
+         success = (0 == chmod(filepath.c_str(), mode)) && success;
+      }
+
+      return success;
+   };
+
+   // lambda function for removing files
+   auto removeFiles = [&maxFiles](const std::string& path) ->bool {
+      bool success = true;
+      for (size_t index = 0; index < maxFiles; ++index) {
+         std::string filepath = {path + "/" + std::to_string(index)};
+         success = (0 == remove(filepath.c_str())) && success;
+      }
+      return success;
+   };
+
+
+   finished.push_back(std::async(std::launch::async, changeChmod, dirChange1, 777));
+   finished.push_back(std::async(std::launch::async, changeChmod, dirChange2, 555));
+   finished.push_back(std::async(std::launch::async, removeFiles, dirRemove));
+
+   for (auto& done : finished) {
+      EXPECT_TRUE(done.get());
    }
-   
-   auto changePermissions [](const std::string& fullPathName) -> bool {
-      return (0 == chmod(fullPathName.c_str(), 777));
+
+   bool success = true;
+   for (size_t index = 0; success && (index < maxFiles); ++index) {
+      EXPECT_FALSE(FileIO::DoesFileExist({dirRemove + "/" + std::to_string(index)}));
+
+
+      std::string changed1 = {dirChange1 + "/" + std::to_string(index)};
+      std::string changed2 = {dirChange2 + "/" + std::to_string(index)};
+      EXPECT_TRUE(FileIO::DoesFileExist(changed1));
+      EXPECT_TRUE(FileIO::DoesFileExist(changed2));
+
+      struct stat buf1;
+      EXPECT_EQ(0,  stat(changed1.c_str(), &buf1));
+      EXPECT_EQ(777, buf1.st_mode & 777);
+      EXPECT_NE(555, buf1.st_mode & 555);
+
+      struct stat buf2;
+      EXPECT_EQ(0, stat(changed2.c_str(), &buf2));
+      EXPECT_EQ(555, buf2.st_mode & 555);
+      EXPECT_NE(777, buf2.st_mode & 777);
    }
-   
-   // Sanity check that empty directories can be deleted
-   EXPECT_TRUE(createDir({mTestDirectory + "/remove_me2"}));
-   EXPECT_TRUE(FileIO::DoesDirectoryExist({mTestDirectory + "/remove_me2"));
-   EXPECT_FALSE(createDir({mTestDirectory + "/remove_me2"}));
-   
-   
-   std::vector<std::future<bool>> asyncDoneStatus;
-   for(size_t index = 0; index < 16000; ++index) {
-      asyncDoneStatus.push_back(std::async(std::launch::async()));
-      
-      
-      
-      
-   }
-   
-//   
-//   for (size_t dirIndex = 0; dirIndex < maxDirectories; ++dirIndex) {
-//      auto dir1 = CreateSubDirectory(std::to_string(dirIndex));
-//      auto dir2 = CreateSubDirectory({"/to_remove/" + std::to_string(dirIndex)});
-//   
-//      CreateFile(dir1, {"f_" + std::to_string(1)});
-//      CreateFile(dir1, {"f_" + std::to_string(2)});
-//   }
-   
-   
-   
-   // Verify setup
-   //EXPECT_TRUE()
-   
 
 }
+
+
