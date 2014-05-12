4fffc67d8aa94b51068a8d45705798e5cd352c99
Robert Weber <robert.weber@logrhythm.com>
2014-05-12 15:50:11 -0600

    Some cleanup

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,16 +13,21 @@ set -x
 cd FileIO
 sh scripts/buildRpm.sh >> /tmp/bootstrap.out 2>&1
 $RPMCOMMAND $RPMARGS /root/rpmbuild/RPMS/x86_64/*.rpm >> /tmp/bootstrap.out 2>&1
+/root/rpmbuild/BUILD/FileIO/UnitTestRunner
+cp /root/rpmbuild/RPMS/*/*.rpm rpms
 cd ..
+
 cd Death
 sh scripts/buildRpm.sh >> /tmp/bootstrap.out 2>&1
 $RPMCOMMAND $RPMARGS /root/rpmbuild/RPMS/x86_64/*.rpm >> /tmp/bootstrap.out 2>&1
-cd ..
+/root/rpmbuild/BUILD/Death/UnitTestRunner
 cp /root/rpmbuild/RPMS/*/*.rpm rpms
-(cd Death/3rdparty ; tar cf - gtest-1.7.0 ) | (cd ZMQ/3rdparty ; tar xf - )
+cd ..
+
 cd ZMQ
 sh scripts/buildRpm.sh >> /tmp/bootstrap.out 2>&1
 $RPMCOMMAND $RPMARGS /root/rpmbuild/RPMS/x86_64/*.rpm >> /tmp/bootstrap.out 2>&1
+/root/rpmbuild/BUILD/Death/UnitTestRunner
 cd ..
 cp /root/rpmbuild/RPMS/*/*.rpm rpms
 
