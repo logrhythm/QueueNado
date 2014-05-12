ae29a9eaafc40cd9c342d3b6021b2621a4228b89
Robert Weber <robert.weber@logrhythm.com>
2014-05-12 15:46:53 -0600

    Almost there

--- Death/3rdparty/test_main.cpp
+++ Death/3rdparty/test_main.cpp
@@ -1,16 +1,29 @@
-/** ==========================================================================
-* 2011 by KjellKod.cc. This is PUBLIC DOMAIN to use at your own risk and comes
-* with no warranties. This code is yours to share, use and modify with no
-* strings attached and no restrictions or obligations.
-* ============================================================================*/
-
+#include <stdio.h>
 #include <gtest/gtest.h>
-#include <iostream>
+#include <gmock/gmock.h>
+#include <g2logworker.hpp>
+#include <g2log.hpp>
+#include <stdlib.h>
+#include <time.h>
+#include <thread>
+#include <google/protobuf/stubs/common.h>
+
+std::string gProgramName;
+std::shared_ptr<g2LogWorker> g2logger(nullptr);
 
 int main(int argc, char *argv[])
 {
-  testing::InitGoogleTest(&argc, argv);
-  int return_value = RUN_ALL_TESTS();
-  std::cout << "FINISHED WITH THE TESTING" << std::endl;
-  return return_value;
+
+   // Start the g2log logger running
+   srand(time(NULL));
+   std::stringstream fileName;
+   fileName << "UnitTest" << geteuid();
+   g2logger.reset(new g2LogWorker(fileName.str(), "/tmp/" ));
+   g2::initializeLogging(g2logger.get());
+
+   ::testing::InitGoogleTest(&argc, argv);
+   int retval = RUN_ALL_TESTS();
+   g2::shutDownLogging();
+   std::this_thread::sleep_for(std::chrono::seconds(5));
+   return retval;
 }
