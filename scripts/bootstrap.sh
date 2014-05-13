9a232e632e44057a28eb2ccb346a876aec3f0e82
Robert Weber <robert.weber@logrhythm.com>
2014-05-13 11:03:57 -0600

    Unit tests from within the buildRpm

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,21 +13,18 @@ set -x
 cd FileIO
 sh scripts/buildRpm.sh >> /tmp/bootstrap.out 2>&1
 $RPMCOMMAND $RPMARGS /root/rpmbuild/RPMS/x86_64/*.rpm >> /tmp/bootstrap.out 2>&1
-/root/rpmbuild/BUILD/FileIO/UnitTestRunner
 cp /root/rpmbuild/RPMS/*/*.rpm rpms
 cd ..
 
 cd Death
 sh scripts/buildRpm.sh >> /tmp/bootstrap.out 2>&1
 $RPMCOMMAND $RPMARGS /root/rpmbuild/RPMS/x86_64/*.rpm >> /tmp/bootstrap.out 2>&1
-/root/rpmbuild/BUILD/Death/UnitTestRunner
 cp /root/rpmbuild/RPMS/*/*.rpm rpms
 cd ..
 
 cd ZMQ
 sh scripts/buildRpm.sh >> /tmp/bootstrap.out 2>&1
 $RPMCOMMAND $RPMARGS /root/rpmbuild/RPMS/x86_64/*.rpm >> /tmp/bootstrap.out 2>&1
-/root/rpmbuild/BUILD/Death/UnitTestRunner
 cd ..
 cp /root/rpmbuild/RPMS/*/*.rpm rpms
 
