a8aa0beff975810ffed6b1448adc4473bd937a48
Yannick.Polius <Yannick.Polius@logrhythm.com>
2016-02-26 15:16:38 -0700

    RifleVampireTests: added new nanomsg test framework

--- test/RifleVampireTests.cpp
+++ test/RifleVampireTests.cpp
@@ -1,97 +1,125 @@
-
 #include <czmq.h>
 #include <boost/thread.hpp>
 #include "RifleVampireTests.h"
 #include "Death.h"
 #include "FileIO.h"
-std::atomic<int> RifleVampireTests::mShotsDeleted;
-
-void TestDeleteString(void*, void* data) {
-   std::string* theString = reinterpret_cast<std::string*> (data);
-   RifleVampireTests::mShotsDeleted++; // yes this is threadsafe
-   delete theString;
-}
-
-std::string RifleVampireTests::GetTcpLocation() {
-   int max = 9000;
-   int min = 7000;
-   int port = (rand() % (max - min)) + min;
-   std::string tcpLocation("tcp://127.0.0.1:");
-   tcpLocation.append(std::to_string(port));
-   return tcpLocation;
-}
-
-std::string RifleVampireTests::GetIpcLocation() {
-   int pid = getpid();
-   std::string ipcLocation("ipc:///tmp/");
-   ipcLocation.append("RifleVampireTests");
-   ipcLocation.append(std::to_string(pid));
-   ipcLocation.append(".ipc");
-   return ipcLocation;
-}
-
-void RifleVampireTests::ShootZeroCopyThread(int numberOfMessages,
-        std::string& location, std::string& exampleData, int hwm, int ioThreads,
-        bool ownSocket) {
-   Rifle rifle(location);
-#ifdef OLD_HAT
-   rifle.SetHighWater(hwm);
-   rifle.SetIOThreads(ioThreads);
-   if (!ownSocket) {
-      rifle.SetOwnSocket(false);
-   } else {
-      rifle.SetOwnSocket(true);
+#include <g3log/g3log.hpp>
+#include <functional>
+namespace {
+   std::string GetTcpLocation() {
+      int max = 9000;
+      int min = 7000;
+      int port = (rand() % (max - min)) + min;
+      std::string tcpLocation("tcp://127.0.0.1:");
+      tcpLocation.append(std::to_string(port));
+      return tcpLocation;
    }
-   rifle.Aim();
-#endif
-   std::stringstream ss;
-   for (int i = 0; i < numberOfMessages && !zctx_interrupted; i++) {
-#ifdef OLD_HAT
-      std::string* zero = new std::string(exampleData);
-      bool result = rifle.FireZeroCopy(zero, zero->size(), TestDeleteString, 10000);
-      if (!result) {
-         std::cout << "failed to fire" << std::endl;
+
+   std::string GetIpcLocation() {
+      int pid = getpid();
+      std::string ipcLocation("ipc:///tmp/");
+      ipcLocation.append("RifleVampireTests");
+      ipcLocation.append(std::to_string(pid));
+      ipcLocation.append(".ipc");
+      return ipcLocation;
+   }
+
+   std::vector<std::shared_ptr<Rifle>> CreateRifles(int howMany,
+                                   const std::string& location) {
+      std::vector<std::shared_ptr<Rifle>> toReturn;
+      for (int i = 0; i < howMany ; i ++) {
+         toReturn.push_back(std::make_shared<Rifle>(location));
       }
-#endif
+      return toReturn;
    }
-   //std::cout << "Done Shooting" << std::endl;
-   while (!zctx_interrupted) {
-      boost::this_thread::sleep(boost::posix_time::seconds(1));
-   }
-
-}
-
-/**
- * Start a vampire thread and pull off the set number of messages.
- * @param numberOfMessages
- * @param location
- * @param exampleData
- * @param hwm
- * @param ioThreads
- * @param ownSocket
- */
-void RifleVampireTests::VampireThread(int numberOfMessages,
-        std::string& location, std::string& exampleData, int hwm,
-        int ioThreads, bool ownSocket) {
-   Vampire vampire(location);
-#ifdef OLD_HAT
-   vampire.SetHighWater(hwm);
-   vampire.SetIOThreads(ioThreads);
-   vampire.SetOwnSocket(ownSocket);
-   ASSERT_TRUE(vampire.PrepareToBeShot());
-#endif
-   for (int i=0; i < numberOfMessages && !zctx_interrupted; i++) {
-      std::string bullet;
-#ifdef OLD_HAT
-      if (vampire.GetShot(bullet, 2000)) {
-         EXPECT_EQ(bullet, exampleData);
-      } else {
-         //no shot in time try again
-         i--;
+   std::vector<std::shared_ptr<Vampire>> CreateVampires(int howMany,
+                                      const std::string& location) {
+      std::vector<std::shared_ptr<Vampire>> toReturn;
+      for (int i = 0; i < howMany ; i ++) {
+         toReturn.push_back(std::make_shared<Vampire>(location));
       }
-#endif
+      return toReturn;
    }
-   while (!zctx_interrupted) {
-      boost::this_thread::sleep(boost::posix_time::seconds(1));
+   std::vector<std::thread> ListenWithVampires(std::vector<std::shared_ptr<Vampire>>& vampires,
+                                               std::function<void(std::shared_ptr<Vampire>)> vampLambda) {
+      std::vector<std::thread> threads;
+      for (auto& vamp:vampires) {
+         threads.push_back(std::thread(vampLambda, vamp));
+      }
+      return threads;
    }
+   std::vector<std::thread> ShootWithRifles(std::vector<std::shared_ptr<Rifle>>& rifles,
+                        std::function<void(std::shared_ptr<Rifle>)> rifleLambda) {
+      std::vector<std::thread> threads;
+      for (auto& rifle:rifles) {
+         threads.push_back(std::thread(rifleLambda, rifle));
+      }
+      return threads;
+   }
+   void FireStringsAtVampires(int howManyRifles,
+                            int howManyShots,
+                            int howManyVampires,
+                            const std::string& location,
+                            const std::string& data) {
+      LOG(DEBUG) << "FireStringsAtVampires";
+      auto rifleList = CreateRifles(howManyRifles, location);
+      auto vampireList = CreateVampires(howManyVampires, location);
+      auto rifleLambda = [&](std::shared_ptr<Rifle> threadGun) {
+         for (int i = 0; i < howManyShots; i++) {
+            threadGun->Fire(data);
+         }
+      };
+      auto vampireLambda = [&](std::shared_ptr<Vampire> threadVamp) {
+         for (int i = 0; i < howManyShots; i++) {
+            auto queueString = threadVamp->GetShot();
+            EXPECT_EQ(queueString, data);
+         }
+      };
+      auto vampThreads = ListenWithVampires(vampireList, vampireLambda);
+      auto rifleThreads = ShootWithRifles(rifleList, rifleLambda);
+      for (auto& thread:vampThreads) {
+         thread.join();
+      }
+      for (auto& thread:rifleThreads) {
+         thread.join();
+      }
+      LOG(DEBUG) << "FireStringsAtVampires finished";
+   }
+}
+TEST_F(RifleVampireTests, VampireTimeout) {
+   const int timeoutInMs = {5};
+   Vampire vamp (GetIpcLocation(), timeoutInMs);
+   EXPECT_THROW(auto received = {vamp.GetShot()}, std::runtime_error);
+}
+TEST_F(RifleVampireTests, TimeoutRifleShouldThrow) {
+   const int timeoutInMs = {5};
+   Rifle rifle(GetIpcLocation(), timeoutInMs);
+   std::string testString("test string");
+   EXPECT_THROW(rifle.Fire(testString), std::runtime_error);
+}
+TEST_F(RifleVampireTests, ipcFilesCleanedOnDelete) {
+   LOG(DEBUG) << "ipcFilesCLeanedOnDelete";
+   auto ipcFile = GetIpcLocation();
+   std::shared_ptr<Rifle> rifle = std::make_shared<Rifle>(ipcFile);
+   std::shared_ptr<Vampire> vamp = std::make_shared<Vampire>(ipcFile);
+   std::string addressRealPath(ipcFile, ipcFile.find("ipc://")+6);
+   ASSERT_TRUE(FileIO::DoesFileExist(addressRealPath));
+   rifle.reset();
+   vamp.reset();
+   ASSERT_FALSE(FileIO::DoesFileExist(addressRealPath));
+   LOG(DEBUG) << "ipcFilesCLeanedOnDelete finished";
+}
+TEST_F(RifleVampireTests, ipcFilesCleanedOnFatal) {
+   LOG(DEBUG) << "ipcFilesCLeanedOnFatal";
+   auto ipcFile = GetIpcLocation();
+   Rifle rifle(ipcFile);
+   Vampire vamp(ipcFile);
+   std::string addressRealPath(ipcFile, ipcFile.find("ipc://")+6);
+   ASSERT_TRUE(FileIO::DoesFileExist(addressRealPath));
+   Death::SetupExitHandler();
+   CHECK(false);
+   LOG(DEBUG) << "recovered from fatal";
+   std::this_thread::sleep_for(std::chrono::milliseconds(500));
+   ASSERT_FALSE(FileIO::DoesFileExist(addressRealPath));
+   LOG(DEBUG) << "ipcFilesCLeanedOnFatal finished";
 }
--- test/RifleVampireTests.h
+++ test/RifleVampireTests.h
@@ -14,33 +14,6 @@ public:
 
    RifleVampireTests() {
    };
-   int mPid;
-   std::string mIpcLocation;
-   std::string mTcpLocation;
-   std::string mInprocLocation;
-   static std::atomic<int> mShotsDeleted;
-   static std::string GetTcpLocation();
-   static std::string GetIpcLocation();
-   static std::string GetInprocLocation();
-   void RifleThread(int numberOfMessages, std::string& location,
-                    std::string& exampleData, int hwm, int ioThreads, bool ownSocket);
-   void VampireThread(int numberOfMessages, std::string& location,
-         std::string& exampleData, int hwm, int ioThreads, bool ownSocket);
-   void ShootZeroCopyThread(int numberOfMessages,
-        std::string& location, std::string& exampleData,int hwm, int ioThreads, 
-        bool ownSocket);
-   void OneRifleNVampiresBenchmark(int nVampires, int nIOThreads,
-           int rifleHWM, int vampireHWM, std::string& location, int dataSize,
-           int nShotsPerVampire, int expectedSpeed);
-   void OneRifleNVampiresStakeBenchmark(int nVampires, int nIOThreads,
-           int rifleHWM, int vampireHWM, std::string& location, int dataSize,
-           int nShotsPerVampire, int expectedSpeed);
-   void NRiflesOneVampireBenchmark(int nRifles, int nIOThreads,
-           int rifleHWM, int vampireHWM, std::string& location, int dataSize,
-           int nShotsPerRifle, int expectedSpeed);
-   void NRiflesOneVampireBenchmarkZeroCopy(int nRifles, int nIOThreads,
-           int rifleHWM, int vampireHWM, std::string& location, int dataSize,
-           int nShotsPerRifle, int expectedSpeed);
 
 protected:
 
@@ -49,75 +22,6 @@ protected:
 
    virtual void TearDown() {   };
 
-   void MakeBigData(size_t size) {
-      t_bigDataSize = size - (size % 4);
-      if (t_bigData)
-         delete t_bigData;
-      t_bigData = new u_char[t_bigDataSize];
-      for (unsigned int i = 0; i < t_bigDataSize / 4; i++) {
-         long data = random();
-         t_bigData[i * 4] = (data >> 24) & 0xff;
-         t_bigData[i * 4 + 1] = (data >> 16) & 0xff;
-         t_bigData[i * 4 + 2] = (data >> 8) & 0xff;
-         t_bigData[i * 4 + 3] = (data) & 0xff;
-      }
-   }
-
-   void MakePacketsData() {
-      long bigNumber = 1500;
-      t_bigDataSize = bigNumber - (bigNumber % 4);
-      if (t_bigData)
-         delete t_bigData;
-      t_bigData = new u_char[t_bigDataSize];
-      for (unsigned int i = 0; i < t_bigDataSize / 4; i++) {
-         long data = random();
-         t_bigData[i * 4] = (data >> 24) & 0xff;
-         t_bigData[i * 4 + 1] = (data >> 16) & 0xff;
-         t_bigData[i * 4 + 2] = (data >> 8) & 0xff;
-         t_bigData[i * 4 + 3] = (data) & 0xff;
-      }
-   }
-
-   void SetExpectedTime(size_t numberOfPackets, size_t packetSize, size_t RateInMbps, size_t transactionsPerSecond) {
-      size_t totalBytes = (numberOfPackets * packetSize);
-      t_totalTransactions = numberOfPackets;
-      t_packetSize = packetSize;
-      t_targetTransactionsPerSecond = transactionsPerSecond;
-      std::cout << std::dec << "Total MBytes: " << totalBytes / (1024 * 1024) << std::endl;
-      t_expectedTime = ((totalBytes * 1.0) / (RateInMbps / 8.0));
-
-      std::cout << "Expected Time :" << t_expectedTime / 1000000.0 << "s at " << RateInMbps << "Mbps" << std::endl;
-   }
-
-   void StartTimedSection() {
-      gettimeofday(&t_startTime, NULL);
-   }
-
-   void EndTimedSection() {
-      gettimeofday(&t_endTime, NULL);
-      t_elapsedUS = (t_endTime.tv_sec - t_startTime.tv_sec)*1000000L + (t_endTime.tv_usec - t_startTime.tv_usec);
-      std::cout << std::dec << "Elapsed Time :" << t_elapsedUS / 1000000L << "." << std::setfill('0') << std::setw(6) << t_elapsedUS % 1000000 << "s" << std::endl;
-      double totalTransactionsPS = (t_totalTransactions * 1.0) / (t_elapsedUS * 1.0 / 1000000.0);
-      std::cout << "Transactions/second :" << totalTransactionsPS << " at ";
-      double totalSpeed = (t_packetSize) * totalTransactionsPS / (125000.0);
-      std::cout << totalSpeed << "Mbps" << std::endl;
-   }
-
-   bool TimedSectionPassed() {
-      return (t_elapsedUS < t_expectedTime);
-   }
-   int mPacketsSeen;
-   int mPacketsToTest;
-   static int gCurrentPacketSize;
-   int t_totalTransactions;
-   int t_targetTransactionsPerSecond;
-   u_char* t_bigData;
-   size_t t_bigDataSize;
-   double t_expectedTime;
-   time_t t_elapsedUS;
-   timeval t_startTime;
-   size_t t_packetSize;
-   timeval t_endTime;
 private:
 
 };
