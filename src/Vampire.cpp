f216db9c7bf3857900eafc526333af0255a6d6aa
Yannick.Polius <Yannick.Polius@logrhythm.com>
2016-02-26 15:15:04 -0700

    Vampire: updated to use nano protocol to guarantee ipc cleanup

--- src/Vampire.cpp
+++ src/Vampire.cpp
@@ -2,38 +2,97 @@
 #include <stdexcept>
 #include <g3log/g3log.hpp>
 /**
- * Construct our Vampire which is a nanomsg pull
+ * Construct a blocking vampire (no timeout)
  */
-Vampire::Vampire(const std::string& location) :
-   mLocation(location) {
+Vampire::Vampire(const std::string& location) {
+   mProtocolHandler = std::move(std::unique_ptr<NanoProtocol>(
+                                   new NanoProtocol(location)));
    mSocket = nn_socket(AF_SP, NN_PULL);
    if (mSocket < 0) {
       throw std::runtime_error("could not open transport socket");
    }
-   auto connect_response = nn_connect(mSocket, location.c_str());
+   auto connect_response = nn_connect(mSocket,
+                                      mProtocolHandler->GetLocation().c_str());
    if (connect_response < 0) {
-      throw std::runtime_error("could not connect to endpoint: " + location);
+      throw std::runtime_error("could not connect to endpoint: " +
+                               mProtocolHandler->GetLocation());
    }
+   LOG(DEBUG) << "opened socket at: " << mProtocolHandler->GetLocation();
 }
-
 /**
- * Return thet location we are going to be shot.
- * @return
+ * Construct a non-blocking Vampire (timeout)
+ */
+Vampire::Vampire(const std::string& location, const int timeoutInMs) {
+   mProtocolHandler = std::move(std::unique_ptr<NanoProtocol>(
+                                   new NanoProtocol(location)));
+   mSocket = nn_socket(AF_SP, NN_PULL);
+   if (mSocket < 0) {
+      throw std::runtime_error("could not open transport socket");
+   }
+   nn_setsockopt(mSocket,
+                 NN_SOL_SOCKET,
+                 NN_RCVTIMEO,
+                 &timeoutInMs,
+                 sizeof (timeoutInMs));
+   auto connect_response = nn_connect(mSocket,
+                                      mProtocolHandler->GetLocation().c_str());
+   if (connect_response < 0) {
+      throw std::runtime_error("could not connect to endpoint: " +
+                               mProtocolHandler->GetLocation());
+   }
+   LOG(DEBUG) << "opened socket at: " << mProtocolHandler->GetLocation()
+              << " with timeout: " << timeoutInMs;
+}
+/**
+ * Return the socket location
  */
 std::string Vampire::GetBinding() const {
-   return mLocation;
+   return mProtocolHandler->GetLocation();
 }
 /**
- * Get shot by the rifle.
+ * Receive a msg from the nanomsg queue, check for errors
+ */
+int Vampire::ReceiveMsg(void * buf) {
+   auto num_bytes_received = nn_recv(mSocket, &buf, NN_MSG, 0);
+   if (num_bytes_received < 0) {
+      auto error = nn_errno ();
+      if (error == ETIMEDOUT) {
+         throw std::runtime_error("timed out receiving message");
+      }
+   }
+   return num_bytes_received;
+}
+/**
+ * Get a string sent by the rifle
  */
 std::string Vampire::GetShot() {
-   void * buf;
-   auto num_bytes_received = nn_recv(mSocket, &buf, NN_MSG, 0);
+   void * buf = {nullptr};
+   auto num_bytes_received = ReceiveMsg(buf);
    std::string nanoString(static_cast<char*>(buf), num_bytes_received);
    nn_freemsg(buf);
    return nanoString;
 }
-
+/**
+ * Gets a void pointer from the nanomsg queue
+ */
+void Vampire::GetStake(void*& stake) {
+   ReceiveMsg(stake);
+}
+/*
+ * Receives a vector of pairs from the nanomsg queue
+ */
+void Vampire::GetStakes(std::vector<std::pair<void*, unsigned int>>& stakes) {
+   void * buf = {nullptr};
+   auto num_bytes_received = ReceiveMsg(buf);
+   auto vectorStart = reinterpret_cast<std::pair<void*, unsigned int>*> (buf);
+   stakes.assign(vectorStart,
+                 vectorStart
+                 + num_bytes_received /  sizeof (std::pair<void*, unsigned int>));
+   nn_freemsg(buf);
+}
+/**
+ * Destructor that closes socket
+ */
 Vampire::~Vampire() {
    nn_close(mSocket);
 }
--- src/Vampire.h
+++ src/Vampire.h
@@ -1,15 +1,22 @@
 #pragma once
-#include <string>
-#include <vector>
+#include <functional>
+#include <memory>
 #include <nanomsg/pipeline.h>
 #include <nanomsg/nn.h>
+#include <string>
+#include <vector>
+#include "NanoProtocol.h"
 class Vampire {
 public:
    explicit Vampire(const std::string& location);
+   Vampire(const std::string& location, const int timeoutInMs);
    std::string GetBinding() const;
    std::string GetShot();
+   void GetStake(void*& stake);
+   void GetStakes(std::vector<std::pair<void*, unsigned int>>& stakes);
    virtual ~Vampire();
 private:
-   std::string mLocation;
+   int ReceiveMsg(void * buf);
+   std::unique_ptr<NanoProtocol> mProtocolHandler;
    int mSocket;
 };
