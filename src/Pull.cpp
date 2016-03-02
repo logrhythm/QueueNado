d1c3c43fb45261256d4017e03b3c74414313033d
Yannick.Polius <Yannick.Polius@logrhythm.com>
2016-03-02 11:44:48 -0700

    Pull:  updated to have errno strings, changed void** to void*&

--- src/Pull.cpp
+++ src/Pull.cpp
@@ -2,14 +2,17 @@
 #include <stdexcept>
 #include <g3log/g3log.hpp>
 /**
- * Construct a blocking vampire (no timeout)
+ * Construct a blocking nanomsg pull (no timeout)
  */
 Pull::Pull(const std::string& location, const bool shouldConnect) {
    mProtocolHandler = std::move(std::unique_ptr<NanoProtocol>(
                                    new NanoProtocol(location)));
    mSocket = nn_socket(AF_SP, NN_PULL);
    if (mSocket < 0) {
-      throw std::runtime_error("could not open transport socket");
+      throw std::runtime_error("could not open transport socket" +
+                               mProtocolHandler->GetLocation() +
+                               " because of error: " +
+                               std::string(nn_strerror(errno)));
    }
    int connectResponse = {0};
    std::string connectionType("");
@@ -24,13 +27,15 @@ Pull::Pull(const std::string& location, const bool shouldConnect) {
    }
    if (connectResponse < 0) {
       throw std::runtime_error("could not connect to endpoint: " +
-                               mProtocolHandler->GetLocation());
+                               mProtocolHandler->GetLocation() +
+                               " because of error: " +
+                               std::string(nn_strerror(errno)));
    }
    LOG(DEBUG) << connectionType
               << " socket at: " << mProtocolHandler->GetLocation();
 }
 /**
- * Construct a non-blocking Pull (timeout)
+ * Construct a non-blocking nanomsg pull (timeout)
  */
 Pull::Pull(const std::string& location,
                  const int timeoutInMs,
@@ -39,7 +44,10 @@ Pull::Pull(const std::string& location,
                                    new NanoProtocol(location)));
    mSocket = nn_socket(AF_SP, NN_PULL);
    if (mSocket < 0) {
-      throw std::runtime_error("could not open transport socket");
+      throw std::runtime_error("could not open transport socket" +
+                               mProtocolHandler->GetLocation() +
+                               " because of error: " +
+                               std::string(nn_strerror(errno)));
    }
    nn_setsockopt(mSocket,
                  NN_SOL_SOCKET,
@@ -59,7 +67,9 @@ Pull::Pull(const std::string& location,
    }
    if (connectResponse < 0) {
       throw std::runtime_error("could not connect to endpoint: " +
-                               mProtocolHandler->GetLocation());
+                               mProtocolHandler->GetLocation() +
+                               " because of error: " +
+                               std::string(nn_strerror(errno)));
    }
    LOG(DEBUG) << connectionType
               << " socket at: " << mProtocolHandler->GetLocation()
@@ -74,8 +84,8 @@ std::string Pull::GetBinding() const {
 /**
  * Receive a msg from the nanomsg queue, check for errors
  */
-int Pull::ReceiveMsg(void ** buf) {
-   auto num_bytes_received = nn_recv(mSocket, buf, NN_MSG, 0);
+int Pull::ReceiveMsg(void *& buf) {
+   auto num_bytes_received = nn_recv(mSocket, &buf, NN_MSG, 0);
    if (num_bytes_received < 0) {
       auto error = nn_errno ();
       if (error == ETIMEDOUT) {
@@ -85,11 +95,11 @@ int Pull::ReceiveMsg(void ** buf) {
    return num_bytes_received;
 }
 /**
- * Get a string sent by the rifle
+ * Get a string sent by the nano push
  */
 std::string Pull::GetString() {
    void * buf = nullptr;
-   auto num_bytes_received = ReceiveMsg(&buf);
+   auto num_bytes_received = ReceiveMsg(buf);
    std::string nanoString(static_cast<char*>(buf), num_bytes_received);
    nn_freemsg(buf);
    return nanoString;
@@ -97,22 +107,22 @@ std::string Pull::GetString() {
 /**
  * Gets a void pointer from the nanomsg queue
  */
-int Pull::GetPointer(void*& stake) {
+int Pull::GetPointer(void*& pointer) {
    void * buf = nullptr;
-   auto bytesReceived = ReceiveMsg(&buf);
+   auto bytesReceived = ReceiveMsg(buf);
    auto castedBuf = reinterpret_cast<void**>(buf);
-   stake = *castedBuf;
+   pointer = *castedBuf;
    nn_freemsg(buf);
    return bytesReceived;
 }
 /**
  * Gets a vector of pointers from the nanomsg queue
  */
-void Pull::GetVector(std::vector<std::pair<void*, unsigned int>>& stakes) {
+void Pull::GetVector(std::vector<std::pair<void*, unsigned int>>& vector) {
    void * buf = {nullptr};
-   auto num_bytes_received = ReceiveMsg(&buf);
+   ReceiveMsg(buf);
    auto receivedVector = reinterpret_cast<std::vector<std::pair<void*, unsigned int>>*> (buf);
-   stakes = *receivedVector;
+   vector = *receivedVector;
    nn_freemsg(buf);
 }
 /**
--- src/Pull.h
+++ src/Pull.h
@@ -10,15 +10,15 @@ class Pull {
 public:
    Pull(const std::string& location, const bool shouldConnect=false);
    Pull(const std::string& location,
-           const int timeoutInMs,
-           const bool shouldConnect=false);
+        const int timeoutInMs,
+        const bool shouldConnect=false);
    std::string GetBinding() const;
    std::string GetString();
-   void GetVector(std::vector<std::pair<void*, unsigned int>>& stakes);
-   int GetPointer(void*& stake);
+   void GetVector(std::vector<std::pair<void*, unsigned int>>& vector);
+   int GetPointer(void*& pointer);
    virtual ~Pull();
 private:
-   int ReceiveMsg(void ** buf);
+   int ReceiveMsg(void *& buf);
    std::unique_ptr<NanoProtocol> mProtocolHandler;
    int mSocket;
 };
